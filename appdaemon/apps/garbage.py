c82687fd9748a69e0146864db1a9ebec87bf64f8
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-01-25 07:11:54 +0100

    Create garbage.py

---
+++ appdaemon/apps/garbage.py
@@ -0,0 +1,15 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+#
+# App to handle garbage topics
+#
+# Args:
+#
+
+class garbage(hass.Hass):
+
+    def initialize(self):
+        self.listen_state(self.check_next_day)
+        
+    def check_next_day(self, entity, attribute, old, new, kwargs):
+        pass
