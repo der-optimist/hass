ec864e00c44c1504da0ff613915a6856f971902f
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-01-30 10:20:39 +0100

    Update garbage.py

--- appdaemon/apps/garbage.py
+++ appdaemon/apps/garbage.py
@@ -9,7 +9,38 @@ import appdaemon.plugins.hass.hassapi as hass
 class garbage(hass.Hass):
 
     def initialize(self):
-        self.listen_state(self.check_next_day)
+        time_check_next_day = datetime.time(17, 00, 0)
+        self.run_daily(self.check_next_day, time_check_next_day)
+        self.listen_state(self.update_waste, "calendar.restmuelltonne", attribute="end_time")
+        # for testing:
+        self.run_minutely(self.update_waste, "calendar.restmuelltonne", attribute="end_time")
         
     def check_next_day(self, entity, attribute, old, new, kwargs):
         pass
+
+    def update_waste(self, entity, attribute, old, new, kwargs):
+        display_text = self.create_text(new)
+        self.set_state("sensor.restmuell_anzeige", state=display_text)
+
+    def update_organic(self, entity, attribute, old, new, kwargs):
+        pass
+
+    def update_paper(self, entity, attribute, old, new, kwargs):
+        pass
+
+    def update_plastic(self, entity, attribute, old, new, kwargs):
+        pass
+
+    def update_all(self, entity, attribute, old, new, kwargs):
+        pass
+
+    def create_text(self, start_date):
+        weekdays = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+        days = (start_date.date() - self.datetime.now().date()).days
+        if days == 0:
+            printtext = "heute"
+        elif days == 1:
+            printtext = "morgen"
+        else:
+            printtext = start_date.strftime('{}, %d.%m. ({} T.)').format(weekdays[start_date.weekday()], days)
+        return printtext
