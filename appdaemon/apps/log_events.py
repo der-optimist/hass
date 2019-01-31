60f7699222fb3cd454b353b399e85016b08b4e09
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-01-31 06:45:39 +0100

    Update log_events.py

--- appdaemon/apps/log_events.py
+++ appdaemon/apps/log_events.py
@@ -1,1 +1,16 @@
-def cycle_scene(self, event_name, data, kwargs):
+import appdaemon.plugins.hass.hassapi as hass
+import datetime
+
+#
+# App to handle garbage topics
+#
+# Args: 
+# 
+
+class log_events(hass.Hass):
+
+    def initialize(self):
+        self.listen_event(self.log_all_events)
+    
+    def log_all_events(self, event_name, data, kwargs):
+        self.log(event_name)
