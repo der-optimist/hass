05a706641eb462bdea1a5736eda9b35f9f0aed80
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-04 10:46:21 +0100

    Update test_cal.py

--- appdaemon/apps/test_cal.py
+++ appdaemon/apps/test_cal.py
@@ -14,11 +14,13 @@ class test_cal(hass.Hass):
     def load_cal(self):
         ha_url = "http://192.168.1.30:8123"
         token = self.args["token"]
-        self.log(token)
+        calendar = "calendar.geburtstage_und_jahrestag"
+        start_date = "2019-02-04T00:00:00"
+        end_sate = "2019-04-04T00:00:00"
         headers = {'Authorization': "Bearer {}".format(token)}
         self.log("Try to load calendars")
-        #apiurl = "{}/api/config".format(ha_url)
-        apiurl = "{}/api/calendars/calendar.geburtstage_und_jahrestag?start=2019-02-04T00:00:00Z&end=2019-04-04T00:00:00Z".format(ha_url)
+        apiurl = "{}/api/calendars/{}?start={}Z&end={}Z".format(ha_url,calendar,start_date,end_date)
         self.log("ha_config: url is {}".format(apiurl))
         r = get(apiurl, headers=headers, verify=False)
         self.log(r.text)
+        self.log(r.json())
