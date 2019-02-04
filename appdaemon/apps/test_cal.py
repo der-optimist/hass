13ae6571a49cba832aa9213d3c6bb539c368b498
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-04 09:41:38 +0100

    Update test_cal.py

--- appdaemon/apps/test_cal.py
+++ appdaemon/apps/test_cal.py
@@ -10,16 +10,16 @@ class test_cal(hass.Hass):
     def initialize(self):
         self.log_cal()
         
-    async def load_cal(self):
+    def load_cal(self):
         conn = aiohttp.TCPConnector()
         self.session = aiohttp.ClientSession(connector=conn)
         ha_url = "http://hassio/homeassistant"
         self.log("Try to load calendars")
         apiurl = "{}/api/config".format(ha_url)
         self.log("ha_config: url is {}".format(apiurl))
-        r = await self.session.get(apiurl, headers=headers, verify_ssl=False)
+        r = self.session.get(apiurl, headers=headers, verify_ssl=False)
         r.raise_for_status()
-        return await r.json()
+        return r.json()
         
     def log_cal(self):
         resp = self.load_cal()
