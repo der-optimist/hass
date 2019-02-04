7745f81d8415b0322b95c01860b786d88ba82f5f
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-04 07:53:51 +0100

    Create test_cal.py

---
+++ appdaemon/apps/test_cal.py
@@ -0,0 +1,24 @@
+import appdaemon.plugins.hass.hassapi as hass
+from requests import get
+
+#
+# testing the hass api
+#
+
+class test_cal(hass.Hass):
+
+    def initialize(self):
+        self.load_cal()
+        
+    def load_cal():
+        self.log("Try to load calendars")
+        token = self.args["token"]
+        auth = 'Bearer ' + token
+        self.log(auth)
+        url = 'http://localhost:8123/api/config'
+        headers = {
+            'Authorization': auth,
+            'content-type': 'application/json',
+        }
+        response = get(url, headers=headers)
+        self.log(response.text)
