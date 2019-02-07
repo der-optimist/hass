680d3250ac1d60262e2a74216437248c08c94c4f
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-07 13:38:28 +0100

    Update calendar_and_reminders.py

--- appdaemon/apps/calendar_and_reminders.py
+++ appdaemon/apps/calendar_and_reminders.py
@@ -46,7 +46,7 @@ class calendar_and_reminders(hass.Hass):
                     self.log("{}: {}".format(_date,summary))
                 else:
                     self.log("No summary in event or no date in start of event - no idea what to do with that one, sorry")
-        self.call_service("variable.set_variable",["birthdays", "birthdays", {"who": summaries, "when": _dates}, None, None])
+        self.call_service("variable/set_variable",variable="birthdays",value="birthdays",attributes={"who": summaries, "when": _dates})
 
 
     def load_calendar(self,calendar,start_dt,end_dt):
