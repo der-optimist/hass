915d6035a7c61f1ba293c68b7211f5f86594ba78
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-11 12:49:47 +0100

    Create toggle_reminder_switches.py

---
+++ appdaemon/apps/toggle_reminder_switches.py
@@ -0,0 +1,24 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+#
+# Some of my apps use switches as reminders (e.g. garbage and calendar_and_reminders)
+# This app handles the events when I click the switch on the UI
+#
+# Args: no args required
+# 
+
+class toggle_reminder_switches(hass.Hass):
+
+    def initialize(self):
+        # --- listen for events of self created switches (they are not handeled by HA)
+        self.listen_event(self.toggle_switches, event = "call_service")
+        
+    def toggle_switches(self,event_name,data, kwargs):
+        entity_id = data["service_data"]["entity_id"]
+        if "switch.reminder" in entity_id:
+            if(data["service"] == "turn_off":
+                self.log(entity_id + " switched off")
+                self.set_state(entity_id, state = "off")
+            if(data["service"] == "turn_on":
+                self.log(entity_id + " switched on")
+                self.set_state(entity_id, state = "on")
