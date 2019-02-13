12b5c6b166a6f6cb8e333c53c7bb9e1f48274540
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-13 08:30:20 +0100

    Update weather_and_astro.py

--- appdaemon/apps/weather_and_astro.py
+++ appdaemon/apps/weather_and_astro.py
@@ -16,9 +16,9 @@ import json
 class weather_and_astro(hass.Hass):
 
     def initialize(self):
-        #self.meteograms_url = "https://nodeserver.cloud3squared.com/getMeteogram/%7B%22chartWidth%22%3A%22800%22%2C%22density%22%3A%221.2%22%2C%22appLocale%22%3A%22de%22%2C%22theme%22%3A%22dark-gradient%22%2C%22provider%22%3A%22dwd.de%22%2C%22hoursToDisplay%22%3A%2284%22%2C%22hoursAvailable%22%3A%2284%22%2C%22headerLocation%22%3A%22false%22%2C%22headerTemperature%22%3A%22false%22%2C%22headerMoonPhase%22%3A%22false%22%2C%22headerUpdateTime%22%3A%22false%22%2C%22precipitationSeries%22%3A%22expected%22%2C%22pressure%22%3A%22false%22%2C%22cloudLayers%22%3A%22false%22%2C%22windSpeed%22%3A%22true%22%2C%22windSpeedMinMaxLabels%22%3A%22false%22%2C%22windSpeedUnit%22%3A%22km%2Fh%22%2C%22windSpeedColor%22%3A%22%23ddc0c0c0%22%2C%22windSpeedAxisMin%22%3A%220%22%2C%22windSpeedAxisMax%22%3A%2240%22%2C%22windSpeedAxisScale%22%3A%22fixed%22%2C%22windArrows%22%3A%22false%22%7D"
         self.meteograms_base_url = "https://nodeserver.cloud3squared.com/getMeteogram/"
         self.meteograms_settings = {
+            "token": "demo_6113b6da87fd72174bb3e5e0f5a",
             "chartWidth": "800",
             "density": "1.2",
             "appLocale": "de",
