3c0885d923fcdbaa19ccd16275ee345f89708e14
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-20 16:16:51 +0100

    Update weather_and_astro.py

--- appdaemon/apps/weather_and_astro.py
+++ appdaemon/apps/weather_and_astro.py
@@ -142,7 +142,7 @@ class weather_and_astro(hass.Hass):
             list_of_active_sensors = []
             for warning in data_sorted:
                 event = warning[5]
-                start_end_readable = "von " + warning[3] + " bis " + warning[4]
+                start_end_readable = warning[3] + " bis " + warning[4]
                 attributes = {"friendly_name": start_end_readable, "Dauer": start_end_readable, "Beschreibung": warning[9], "Stärke (0-4)": warning[0]}
                 sensor_name = "sensor.dwd_warn_" + event.lower() + "_" + warning[1].replace("-","_").replace(":","_").replace("T","_").replace("Z","")
                 if (self.get_state(sensor_name) != event) or (self.get_state(sensor_name, attribute = "Stärke (0-4)") != warning[0]):
