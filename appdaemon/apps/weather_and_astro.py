e50a08fdd5379a374b97b85ce52718806b23bffc
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-20 15:33:00 +0100

    Update weather_and_astro.py

--- appdaemon/apps/weather_and_astro.py
+++ appdaemon/apps/weather_and_astro.py
@@ -143,9 +143,11 @@ class weather_and_astro(hass.Hass):
             for warning in data_sorted:
                 event = warning[5]
                 attributes = {"friendly_name": event, "von": warning[3], "bis": warning[4], "Beschreibung": warning[9], "Stärke (0-4)": warning[0]}
-                sensor_name = "sensor.dwd_warn_" + event.lower() + "_" + warning[1].replace("-","_").replace(":","_").replace("T","_").replace("Z","_1")
-                if (self.get_state(sensor_name) == event) and (self.get_state(sensor_name, attribute = "Stärke (0-4)") == warning[0]):
+                sensor_name = "sensor.dwd_warn_" + event.lower() + "_" + warning[1].replace("-","_").replace(":","_").replace("T","_").replace("Z","_2")
+                if (self.get_state(sensor_name) != event) or (self.get_state(sensor_name, attribute = "Stärke (0-4)") != warning[0]):
                     self.log("Sensor {} scheint neu zu sein".format(sensor_name))
+                    if warning[0] >= 1: # Severity
+                        self.notify("[Warnung] {} (Stärke: {})".format(warning[9],warning[0]), name = "telegram_jo")
                 else:
                     self.log("Sensor {} ist wohl nicht neu".format(sensor_name))
                 self.set_state(sensor_name, state = event, attributes = attributes)
