4f82a0ada97417644336e2058269d66b133c07a2
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-21 10:10:10 +0100

    Update notify_work_arrived.py

--- appdaemon/apps/notify_work_arrived.py
+++ appdaemon/apps/notify_work_arrived.py
@@ -19,11 +19,12 @@ class notify_work_arrived(hass.Hass):
         time_at_work = self.get_state("sensor.jo_at_work")
         self.log("Time at work: {}".format(time_at_work))
         if time_at_work == "0.0":
-            self.notify("Angekommen", name = "telegram_jo")
+            self.fire_event("custom_notify", message="Angekommen", target="telegram_jo")
+            #self.notify("Angekommen", name = "telegram_jo")
 
     def left(self, entity, attribute, old, new, kwargs):
         if new != self.args["zone"]:
             self.log("Jo left work")
             time_at_work_hm = self.get_state("sensor.jo_at_work", attribute="value")
             self.log("Time at work: {}".format(time_at_work_hm))
-            self.notify("Feierabend! Reicht auch, nach {}".format(time_at_work_hm), name = "telegram_jo")
+            self.fire_event("custom_notify", message="Feierabend! Reicht auch, nach {}".format(time_at_work_hm), target="telegram_jo")
