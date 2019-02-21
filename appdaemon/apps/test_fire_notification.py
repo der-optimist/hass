f40ce06549341367f60a180f2eaaa6ea35932fed
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-21 07:55:37 +0100

    Create test_fire_notification.py

---
+++ appdaemon/apps/test_fire_notification.py
@@ -0,0 +1,11 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+#
+# App to test "reliable notification" app
+# 
+#
+
+class test_fire_notification(hass.Hass):
+
+    def initialize(self):
+        self.fire_event("custom_notify", message="My Test Message 1", target="telegram_jo")
