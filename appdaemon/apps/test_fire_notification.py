df468ccf29b6e35c5a415d8f03b04de72aa7b4cf
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-02-21 10:04:19 +0100

    Update test_fire_notification.py

--- appdaemon/apps/test_fire_notification.py
+++ appdaemon/apps/test_fire_notification.py
@@ -8,5 +8,5 @@ import appdaemon.plugins.hass.hassapi as hass
 class test_fire_notification(hass.Hass):
 
     def initialize(self):
-        self.fire_event("custom_notify", message="My Test Message 8", target="telegram_jo")
+        self.fire_event("custom_notify", message="short", target="telegram_jo")
         #self.fire_event("custom_notify", message="My Test Message 7", target="telegram_jo")
