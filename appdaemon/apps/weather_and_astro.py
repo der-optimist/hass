a950cd9d24b8cdfd249793e1098baead177661b8
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-05-14 08:48:13 +0200

    DWD Warn: Switched Name and State

--- appdaemon/apps/weather_and_astro.py
+++ appdaemon/apps/weather_and_astro.py
@@ -153,16 +153,15 @@ class weather_and_astro(hass.Hass):
                 event = warning[5]
                 start_end_readable = warning[3] + " bis " + warning[4]
                 icon = Icons_dict.get(warning[0],"/local/icons/reminders/exclamation_mark_blink.svg")
-                attributes = {"friendly_name": start_end_readable, "entity_picture": icon, "Dauer": start_end_readable, "Beschreibung": warning[9], "Gefahr (0-4)": warning[0]}
+                attributes = {"friendly_name": event, "entity_picture": icon, "Dauer": start_end_readable, "Beschreibung": warning[9], "Gefahr (0-4)": warning[0]}
                 sensor_name = "sensor.dwd_warn_" + event.lower().replace(" ","_").replace("-","_").replace("ä","ae").replace("ö","oe").replace("ü","ue").replace("ß","ss") + "_" + warning[1].replace("-","_").replace(":","_").replace("T","_").replace("Z","")
-                if (self.get_state(sensor_name) != event) or (self.get_state(sensor_name, attribute = "Gefahr (0-4)") != warning[0]):
+                if (self.get_state(sensor_name) != start_end_readable) or (self.get_state(sensor_name, attribute = "Gefahr (0-4)") != warning[0]):
                     self.log("Sensor {} scheint neu zu sein".format(sensor_name))
                     if warning[0] >= 1: # Severity
                         self.fire_event("custom_notify", message="Warnung - {}:\n{}\nGefahr (0-4): {}".format(start_end_readable,warning[9],warning[0]), target="telegram_jo")
-                        #self.notify("Warnung: {} (Stärke (0-4): {})".format(warning[9],warning[0]), name = "telegram_jo")
                 #else:
                     #self.log("Sensor {} ist wohl nicht neu".format(sensor_name))
-                self.set_state(sensor_name, state = event, attributes = attributes)
+                self.set_state(sensor_name, state = start_end_readable, attributes = attributes)
                 list_of_active_sensors.append(sensor_name)
             
             # set outdated sensors to "off"
