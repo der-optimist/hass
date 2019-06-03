5a31bb57533a7536d9330335c4209de9de5ce434
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-06-03 13:28:21 +0200

    Update telegram_bot.py

--- appdaemon/apps/telegram_bot.py
+++ appdaemon/apps/telegram_bot.py
@@ -58,10 +58,10 @@ class telegram_bot(hass.Hass):
         # --- Status Conversation ---
         if text.lower().startswith("status"):
             t = self.conv_handler_curr_type[user_id]
-            s = self.conv_handler_curr_msg_num[user_id]
+            c = self.self.conv_handler_curr_commands[user_id]
             self.call_service('telegram_bot/send_message',
                       target=chat_id,
-                      message="Step: {}\nType: {}".format(s,t))
+                      message="Commands: {}, {}, {}\nType: {}".format(c[0],c[1],c[2],t))
         
         # --- Reset Conversation ---
         if text.lower().startswith("reset"):
