067af59569f2a88626f0d90915ff66a3c69ee83b
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-06-03 14:38:03 +0200

    Update telegram_bot.py

--- appdaemon/apps/telegram_bot.py
+++ appdaemon/apps/telegram_bot.py
@@ -192,7 +192,7 @@ class telegram_bot(hass.Hass):
         self.log("fire the command!")
         commands = self.conv_handler_curr_commands[user_id]
         if self.conv_handler_curr_type[user_id] == 2:
-            value = commmands[2]
+            value = commands[2]
             device = self.conversations["twosteps"][commands[0]]["steps"][commands[1]]["decive"]
         reply = "OK. Gerät {} bekommt den Befehl {}.".format(device,value)
         self.reset_conversation_commands(self, user_id)
