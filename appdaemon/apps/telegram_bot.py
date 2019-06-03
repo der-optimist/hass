cfb2abd22a0e5f2db43f96583f02472eaa9735ca
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-06-03 14:26:50 +0200

    Update telegram_bot.py

--- appdaemon/apps/telegram_bot.py
+++ appdaemon/apps/telegram_bot.py
@@ -132,11 +132,18 @@ class telegram_bot(hass.Hass):
                     inline_keyboard=reply["keyboard"])
             else:
                 if self.conv_handler_curr_commands[user_id][2] == 0:
-                    # choice 2 wurde wohl getroffen => verarbeiten
+                    # choice 2 wurde wohl getroffen => Fuehe Befehl aus
                     commands = self.conv_handler_curr_commands[user_id]
                     commands[2] = text
                     self.conv_handler_curr_commands.update( {user_id : commands} )
                 reply = self.run_command_from_conversation(user_id, chat_id)
+                self.call_service('telegram_bot/answer_callback_query',
+                              message="",
+                              callback_query_id=callback_id,
+                              show_alert=False)
+                self.call_service('telegram_bot/send_message',
+                          target=chat_id,
+                          message=reply)
                 
 
     def send_temps(self, chat_id):
