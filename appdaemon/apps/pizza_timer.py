aa7ce88a1c778dfc5d4bd1c8e1cad7a29afa17f9
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-06-18 10:37:03 +0200

    Update pizza_timer.py

--- appdaemon/apps/pizza_timer.py
+++ appdaemon/apps/pizza_timer.py
@@ -11,7 +11,25 @@ class pizza_timer(hass.Hass):
 
     def initialize(self):
         self.listen_state(self.state_change, "input_select.pizza_timer")
+        self.timer_handle = None
     
     def state_change(self, entity, attributes, old, new, kwargs):
-        if new != None:
-            self.log("State Change in Pizza Timer erkannt: {}".format(new))
+        self.log("State Change in Pizza Timer erkannt: {}".format(new))
+        if new == "keine Pizza":
+            self.log("Schade, keine Pizza")
+        elif new == "Abbruch":
+            if self.timer_handle == None:
+                self.log("Pizza-Timer Abbruch angefragt, aber kein Timer vorhanden")
+            else:
+                self.cancel_timer(self.timer_handle)
+                self.timer_handle = None
+                self.log("Pizza-Timer abgebrochen")
+        else:
+            if self.timer != None:
+                self.cancel_timer(self.timer_handle)
+            self.starttime = datetime.datetime.now()
+            delay_sec = int(new)
+            self.timer = self.run_in(self.remind_pizza,delay_sec)
+            
+    def remind_pizza(self, kwargs):
+        self.log("Pizza ist fertig")
