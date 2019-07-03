cb6480e06b0fc3cbb4d914276db23ff2bf9a7b98
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-07-03 07:23:15 +0200

    Update test.py

--- appdaemon/apps/test.py
+++ appdaemon/apps/test.py
@@ -9,7 +9,7 @@ import appdaemon.plugins.hass.hassapi as hass
 class test(hass.Hass):
 
     def initialize(self):
-        self.listen_state(self.state_change, "switch.dummy_light_switch")
+        self.listen_state(self.state_change, "input_boolean.dummy_light_switch")
     
     def state_change(self, entity, attributes, old, new, kwargs):
         self.log("Schalter betätigt: {}".format(new))
