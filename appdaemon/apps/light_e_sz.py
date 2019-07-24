592ba8c3e671958b6b968c240f1b888146c6a495
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-07-24 10:23:25 +0200

    Update light_e_sz.py

--- appdaemon/apps/light_e_sz.py
+++ appdaemon/apps/light_e_sz.py
@@ -25,13 +25,23 @@ class light_e_sz(hass.Hass):
         else:
             self.log("Bewegung im SZ, keiner schlaeft, werde schauen ob es zu dunkel ist")
             if self.get_state("sensor.helligkeit_schlafzimmer_pm") < self.min_illuminance:
-                self_turn_on("lights.panels_schlafzimmer", brightness_pct=self.brightness_day)
+                self.turn_on("lights.panels_schlafzimmer", brightness_pct=self.brightness_day)
                 self.log("War zu dunkel, habe Licht angemacht")
             else:
                 self.log("War hell genug, habe das Licht aus gelassen")
 
     def presence_off(self, entity, attributes, old, new, kwargs):
         self.log("PM SZ Bett: {}".format(new))
+        self.turn_off("lights.panels_schlafzimmer")
 
     def illuminance_changed(self, entity, attributes, old, new, kwargs):
         self.log("Helligkeit SZ: {}".format(new))
+        if self.get_state(switch.ma_schlaft) == "on" or self.get_state(switch.jo_schlaft) == "on":
+            self.log("Ma oder Jo schlaeft. Werde kein Licht anmachen, egal ob Bewegung oder nicht")
+        else:
+            self.log("Es ist wohl dunkel, keiner schlaeft, werde schauen ob jemand im Raum ist")
+            if self.get_state("binary_sensor.PM_E_SZ_Bett") == "on":
+                self.turn_on("lights.panels_schlafzimmer", brightness_pct=self.brightness_day)
+                self.log("War zu dunkel, es ist jemand da, keiner schlaeft, habe Licht angemacht")
+            else:
+                self.log("Es ist zwar dunkel und keiner schlaeft, aber es ist niemand da. Habe das Licht aus gelassen")
