20cefc38fa4963fa8143b7e290cb3d4976dfe2e1
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-08-27 13:26:29 +0200

    Create wall_panel.py

---
+++ appdaemon/apps/wall_panel.py
@@ -0,0 +1,29 @@
+import appdaemon.plugins.hass.hassapi as hass
+import datetime
+
+#
+# What it does:
+#   - Send a wake command to the wall panel (during day time) for keeping it on
+# 
+
+class weather_and_astro(hass.Hass):
+
+    def initialize(self):
+        self.start_time = time(5,00)
+        self.end_time = time(22,45)
+        self.run_every(self.send_wake_command, datetime.datetime.now(), 5 * 60) # run every 5 minutes
+
+    def send_wake_command(self, kwargs):
+        if is_time_between(self.start_time, self.end_time):
+            self.log("Daytime - will send wake command to panel")
+            self.call_service("mqtt/publish", topic = "wallpanel/mywallpanel/command", payload = "{\"wake\":true,\"wakeTime\":610}", qos = "1")
+        else:
+            self.log("Nighttime - will let the panel sleep")
+        
+    def is_time_between(begin_time, end_time, check_time=None):
+        # If check time is not given, default to current time
+        check_time = check_time or datetime.now().time()
+        if begin_time < end_time:
+            return check_time >= begin_time and check_time <= end_time
+        else: # crosses midnight
+            return check_time >= begin_time or check_time <= end_time
