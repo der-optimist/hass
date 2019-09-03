4570aa950f7611c09d59f6700c02656d455409f7
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-09-03 09:28:19 +0200

    Update notifications.py

--- appdaemon/apps/notifications.py
+++ appdaemon/apps/notifications.py
@@ -20,4 +20,4 @@ class notifications(hass.Hass):
         temp_wz = self.get_state("sensor.0x00158d00034d1e34_temperature")
         lf_wz = self.get_state("sensor.0x00158d00034d1e34_humidity")
         temp_aussen = self.get_state("sensor.temp_owm")
-        self.fire_event("custom_notify", message="===== Temperaturen =====\nWohnzimmer: {} °C ({}%)\nDraussen: {} °C".format(temp_wz,lf_wz,temp_aussen), target="telegram_jo")
+        self.fire_event("custom_notify", message="=== 🔥 Temperaturen ❄️ ===\nWohnzimmer: {} °C ({}%)\nDraussen: {} °C".format(temp_wz,lf_wz,temp_aussen), target="telegram_jo")
