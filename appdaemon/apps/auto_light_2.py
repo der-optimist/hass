a53cf6eb7233cbc5de769e21c6acba971a96a2c7
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-10-10 08:15:03 +0200

    Update auto_light_2.py

--- appdaemon/apps/auto_light_2.py
+++ appdaemon/apps/auto_light_2.py
@@ -19,15 +19,32 @@ class auto_light_2(hass.Hass):
         self.light: str = self.args.get("light")
         self.triggers: Set[str] = self.args.get("triggers", set())
         self.illuminance_sensor: str = self.args.get("illuminance_sensor", None)
+        # brightness depending on time
         self.times_brightness_strings = self.args["brightness_values"].keys()
-        self.find_basic_brightness_value(None)
+        self.update_basic_brightness_value(None)
+        for each in sorted(self.times_brightness_strings):
+            self.run_daily(self.update_basic_brightness_value, datetime.time(int(each.split(":")[0]), int(each.split(":")[1]), 0))
+        # min illuminance depending on time
+        self.times_min_illuminance_strings = self.args["min_illuminance_values"].keys()
+        self.update_min_illuminance_value(None)
+        for each in sorted(self.times_min_illuminance_strings):
+            self.run_daily(self.update_min_illuminance_value, datetime.time(int(each.split(":")[0]), int(each.split(":")[1]), 0))
 
-    def find_basic_brightness_value(self, kwargs):
+    def update_basic_brightness_value(self, kwargs):
         now = datetime.datetime.now()
         for each in sorted(self.times_brightness_strings):
-            if now >= now.replace(hour=int(each.split(":")[0]), minute=int(each.split(":")[1]), second=0, microsecond=0):
-                basic_brightness = self.args["brightness_values"][each]
-        return basic_brightness
+            if now >= now.replace(hour=int(each.split(":")[0]), minute=int(each.split(":")[1])):
+                current_brightness = self.args["brightness_values"][each]
+        self.basic_brightness = current_brightness
+        self.log("Basic brightness set to {}".format(self.basic_brightness))
+
+    def update_min_illuminance_value(self, kwargs):
+        now = datetime.datetime.now()
+        for each in sorted(self.times_min_illuminance_strings):
+            if now >= now.replace(hour=int(each.split(":")[0]), minute=int(each.split(":")[1])):
+                current_min_illuminance = self.args["min_illuminance_values"][each]
+        self.min_illuminance = current_min_illuminance
+        self.log("Min illuminance set to {}".format(self.min_illuminance))
 
     def pct_to_byte(self, val_pct):
         return float(round(val_pct*255/100))
