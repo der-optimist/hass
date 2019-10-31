9e819bdeb330ce87a97a37ffac8e2cd7ed03af6a
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-10-31 14:52:43 +0100

    Update permanent_recorder.py

--- appdaemon/apps/permanent_recorder.py
+++ appdaemon/apps/permanent_recorder.py
@@ -1,5 +1,5 @@
 import appdaemon.plugins.hass.hassapi as hass
-import mysql.connector
+from influxdb import InfluxDBClient
 
 # Saves a state or an attribute to a mysql db (like recorder, but less data => permanent)
 #
@@ -13,55 +13,24 @@ class permanent_recorder(hass.Hass):
 
     def initialize(self):
         self.log("Permanent Logger started")
-        self.mydb = mysql.connector.connect(
-            host="core-mariadb",
-            user="appdaemon",
-            passwd=self.args["db_passwd"],
-            database="homeassistantpermanent"
-        )
+        self.host = self.args.get("host", "core-mariadb")
+        self.port=8086
+        self.user = self.args.get("user", "appdaemon")
+        self.password = self.args.get("db_passwd", None)
+        self.dbname = self.args.get("dbname", "homeassistant_permanent")
         
-        self.init_table()
+        self.client =InfluxDBClient(self.host, self.port, self.user, self.password, self.dbname)
+        
         self.write_test1()
         self.write_test2()
         self.query_test()
         
-
-    def init_table(self):
-        mycursor = self.mydb.cursor()
-        mycursor.execute("SHOW TABLES")
-        table_exists = False
-        for x in mycursor:
-            if "ha" in x:
-                table_exists = True
-        if table_exists:
-            self.log("Table ha in database found. Will use it")
-        else:
-            self.log("Did not find table ha in database. Will create it")
-            #mycursor.execute("CREATE TABLE ha (id INT AUTO_INCREMENT PRIMARY KEY, domain VARCHAR(255), entity_id VARCHAR(255), value VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
-    
     def write_test1(self):
         self.log("Write Test 1")
-        mycursor = self.mydb.cursor()
-        sql = "INSERT INTO ha (domain, entity_id, value) VALUES (%s, %s, %s)"
-        val = ("Test-Domain", "Test-Entity", "on")
-        mycursor.execute(sql, val)
-        self.mydb.commit()
+        self.client.write("Test-Entity brightness=25", protocol='line')
         self.log("Write Test 1 done")
         
     def write_test2(self):
         self.log("Write Test 2")
-        mycursor = self.mydb.cursor()
-        sql = "INSERT INTO ha (domain, entity_id, value) VALUES (%s, %s, %s)"
-        val = ("Test-Domain 2", "Test-Entity 2", 25)
-        mycursor.execute(sql, val)
-        self.mydb.commit()
+        self.client.write("Test-Entity brightness=on", protocol='line')
         self.log("Write Test 2 done")
-        
-    def query_test(self):
-        self.log("Query Test")
-        mycursor = self.mydb.cursor()
-        mycursor.execute("SELECT * FROM ha")
-        myresult = mycursor.fetchall()
-        for x in myresult:
-            self.log(x)
-        self.log("Query Test done")
