72617b8216b68d7576a5e4421cc2dcd639015d10
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-10-31 07:03:08 +0100

    Update wall_panel.py

--- appdaemon/apps/wall_panel.py
+++ appdaemon/apps/wall_panel.py
@@ -20,12 +20,12 @@ class wall_panel(hass.Hass):
             # via mqtt
             self.call_service("mqtt/publish", topic = "wallpanel/mywallpanel/command", payload = "{\"wake\":true,\"wakeTime\":610}", qos = "1")
             # via REST api
-            data = {"wake": true, "wakeTime": 610}
-            headers = {"content-type": "application/json"}
-            try:
-                requests.post(self.url, headers=headers, json=data)
-            except Exception as e:
-                self.log("Error sending wake command to wallpanel via REST api. Error was {}".format(e))
+            #data = {"wake": true, "wakeTime": 610}
+            #headers = {"content-type": "application/json"}
+            #try:
+            #    requests.post(self.url, headers=headers, json=data)
+            #except Exception as e:
+            #    self.log("Error sending wake command to wallpanel via REST api. Error was {}".format(e))
         #else:
             #self.log("Nighttime - will let the panel sleep")
         
