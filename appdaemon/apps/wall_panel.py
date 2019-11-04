8bb84b9facdecc6311999d8909e5386d6621b96f
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-11-04 08:07:33 +0100

    Update wall_panel.py

--- appdaemon/apps/wall_panel.py
+++ appdaemon/apps/wall_panel.py
@@ -1,50 +1,52 @@
 import appdaemon.plugins.hass.hassapi as hass
-import datetime
 import requests
 
 #
 # What it does:
-#   - Send a wake command to the wall panel (during day time) for keeping it on
+#   - Send a wake command to the wall panel for keeping it on (when presence)
+#   - Send a reload command after wall panel disconnect or HA restart
 # 
 
 class wall_panel(hass.Hass):
 
     def initialize(self):
-        self.start_time = datetime.time(5,00)
-        self.end_time = datetime.time(22,45)
+        # URL for REST api commands
         self.url = "http://192.168.178.26:2971/api/command"
-        self.run_every(self.send_wake_command, datetime.datetime.now(), 5 * 60) # run every 5 minutes
+        # presence
+        self.listen_state(self.presence_on, "binary_sensor.anwesenheit_bildschirm", new = "on")
+        self.listen_state(self.presence_off, "binary_sensor.anwesenheit_bildschirm", new = "off")
+        if self.get_state("binary_sensor.anwesenheit_bildschirm") == "on":
+            self.send_wake_command()
+        # reload page
         self.listen_state(self.wp_online, "binary_sensor.ping_bildschirm", new = "on")
+        #self.run_in(self.send_reload_command, 120) # auskommentiert wegen restart problem
+        self.timer_handle = None
 
-    def send_wake_command(self, kwargs):
-        if self.is_time_between(self.start_time, self.end_time):
-            # via mqtt
-            #self.call_service("mqtt/publish", topic = "wallpanel/mywallpanel/command", payload = "{\"wake\":true,\"wakeTime\":610}", qos = "1")
-            # via REST api
-            try:
-                r = requests.post(self.url, json={"wake":"true","wakeTime":610}, timeout=5)
-                #self.log(r)
-                #self.log(r.text)
-            except Exception as e:
-                self.log("Error sending wake command to wallpanel via REST api. Error was {}".format(e))
-        #else:
-            #self.log("Nighttime - will let the panel sleep")
+    def send_wake_command(self):
+        try:
+            requests.post(self.url, json={"wake":"true","wakeTime":610}, timeout=5)
+        except Exception as e:
+            self.log("Error sending wake command to wallpanel via REST api. Error was {}".format(e))
+        self.timer_handle = self.run_in(self.send_wake_command,60)
+    
+    def send_reload_command(self, kwargs):
+        self.log("Sending reload command to wall panel")
+        try:
+            requests.post(self.url, json={"reload":"true"}, timeout=5)
+        except Exception as e:
+            self.log("Error sending reload command to wallpanel via REST api. Error was {}".format(e))
+
+    def presence_on(self, entity, attributes, old, new, kwargs):
+        self.log("Wall panel presence on. Will send periodic wake commands")
+        self.send_wake_command()
+    
+    def presence_off(self, entity, attributes, old, new, kwargs):
+        self.log("Wall panel presence off. Will cancel periodic wake commands")
+        if self.timer_handle != None:
+            self.cancel_timer(self.timer_handle)
+            self.log("Canceled periodic wake commands")
         
     def wp_online(self, entity, attributes, old, new, kwargs):
         if old != "on":
             self.log("Wall Panel was offline, is online now. Will send reload command")
-            # via REST api
-            try:
-                r = requests.post(self.url, json={"reload":"true"}, timeout=5)
-                #self.log(r)
-                #self.log(r.text)
-            except Exception as e:
-                self.log("Error sending reload command to wallpanel via REST api. Error was {}".format(e))
-        
-    def is_time_between(self, begin_time, end_time, check_time=None):
-        # If check time is not given, default to current time
-        check_time = check_time or datetime.datetime.now().time()
-        if begin_time < end_time:
-            return check_time >= begin_time and check_time <= end_time
-        else: # crosses midnight
-            return check_time >= begin_time or check_time <= end_time
+            self.send_reload_command(None)
