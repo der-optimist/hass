1a540d645ea0c366ca1529b4aed7a512dd93b567
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-11-28 11:28:55 +0100

    Create heating_winter_mode.py

---
+++ appdaemon/apps/heating_winter_mode.py
@@ -0,0 +1,28 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+#
+# Set KNX Winter Switch According to HA Input Boolean 
+#
+# Args:
+# - input_booloean_entity
+# - switch_entity
+# 
+
+class heating_winter_mode(hass.Hass):
+
+    def initialize(self):
+        # wait for KNX entities 
+        self.run_in(self.initialize_delayed,110)
+        self.listen_state(self.input_changed, self.args["input_booloean_entity"])
+    
+    def initialize_delayed(self, kwargs):
+        if self.get_state(self.args["input_booloean_entity"]) == "on":
+            self.turn_on(self.args["switch_entity"])
+        if self.get_state(self.args["input_booloean_entity"]) == "off":
+            self.turn_off(self.args["switch_entity"])
+    
+    def input_changed(self,event_name,data,kwargs):
+        if new == "on":
+            self.turn_on(self.args["switch_entity"])
+        if new == "off":
+            self.turn_off(self.args["switch_entity"])
