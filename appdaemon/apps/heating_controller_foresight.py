9f3d88a01df4db29d03ec23f81bc130c7c75afc7
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-12-03 13:14:02 +0100

    Update heating_controller_foresight.py

--- appdaemon/apps/heating_controller_foresight.py
+++ appdaemon/apps/heating_controller_foresight.py
@@ -118,7 +118,7 @@ class heating_controller_foresight(hass.Hass):
             return
         der_list = []
 
-        query = 'SELECT last("{}") FROM "homeassistant_permanent"."autogen"."{}" WHERE time > now() - 24h ORDER BY time DESC LIMIT 5'.format(self.db_field, self.db_measurement)
+        query = 'SELECT "{}" FROM "homeassistant_permanent"."autogen"."{}" WHERE time > now() - 24h ORDER BY time DESC LIMIT 5'.format(self.db_field, self.db_measurement)
         #self.log(query)
         result_points = self.client.query(query).get_points()
         for point in result_points:
