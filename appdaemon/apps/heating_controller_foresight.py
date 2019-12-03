2aeafc10ee62fca72dd513f7adda25d7e7fd0737
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-12-03 15:19:35 +0100

    Update heating_controller_foresight.py

--- appdaemon/apps/heating_controller_foresight.py
+++ appdaemon/apps/heating_controller_foresight.py
@@ -111,23 +111,25 @@ class heating_controller_foresight(hass.Hass):
             self.call_service("knx/send", address = self.args.get("ga_setpoint_shift"), payload = [value_byte])
 
     def beta(self, kwargs):
-        try:
-            current_value = float(self.get_state(self.db_measurement))
-        except:
-            self.log("Error converting State of {} to float".format(self.db_measurement))
-            return
         der_list = []
         query = 'SELECT "{}" FROM "homeassistant_permanent"."autogen"."{}" WHERE time > now() - 24h ORDER BY time DESC LIMIT 5'.format(self.db_field, self.db_measurement)
         #self.log(query)
         result_points = self.client.query(query).get_points()
+        prev_value = None
+        prev_time = None
         for point in result_points:
             self.log(point)
-            self.log(type(point["time"]))
-            #historic_value = point["last"]
-            #derivative = (current_value - historic_value) / (minutes / 60)
-            #der_list.append(derivative)
-            #self.log(derivative)
-            #break
+            #self.log(type(point["time"]))
+            if prev_value != None:
+                delta_value = point[self.db_field] - prev_value
+                delta_time = datetime.datetime.strptime(point["time"], '%Y-%m-%dT%H:%M:%S.%f') - prev_time
+                delta_time_seconds = delta_time.total_seconds()
+                derivative = delta_value / (delta_time_seconds / 3600)
+                der_list.append(derivative)
+                self.log(derivative)
+            prev_value = point[self.db_field]
+            prev_time = datetime.datetime.strptime(point["time"], '%Y-%m-%dT%H:%M:%S.%f')
+
     
     def on_off_switch(self, entity, attribute, old, new, kwargs):
         if new == "off" and old != new:
