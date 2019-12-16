d897426a82da711e787995b56561c039a1501688
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-12-16 14:54:43 +0100

    Create test_cube.py

---
+++ appdaemon/apps/test_cube.py
@@ -0,0 +1,18 @@
+import appdaemon.plugins.hass.hassapi as hass
+#
+# App does:
+#  - Send notification on status change of aqara cube (testing)
+#
+
+class test_cube(hass.Hass):
+
+    def initialize(self):
+        self.listen_state(self.action, "sensor.cube_1_action")
+        self.listen_state(self.side, "sensor.cube_1_side")
+        
+    def action(self, entity, attribute, old, new, kwargs):
+        side = self.get_state("sensor.cube_1_side")
+        self.fire_event("custom_notify", message="Cube Action changed: {} / Side zu dem Zeitpunkt: {}".format(new,side), target="telegram_jo")
+
+    def side(self, entity, attribute, old, new, kwargs):
+        self.fire_event("custom_notify", message="Cube Side changed: {}".format(new), target="telegram_jo")
