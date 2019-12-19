b2de2516837cd4072815b60994df0151a1632b93
der-optimist <45357614+der-optimist@users.noreply.github.com>
2019-12-19 11:00:47 +0100

    Update supervise_knx_sensor.py

--- appdaemon/apps/supervise_knx_sensor.py
+++ appdaemon/apps/supervise_knx_sensor.py
@@ -11,13 +11,15 @@ import appdaemon.plugins.hass.hassapi as hass
 class supervise_knx_sensor(hass.Hass):
 
     def initialize(self):
-        self.timer_handle = self.run_in(self.remind_me,60*self.args["minutes"])
+        self.timer_handle = self.run_in(self.notify_me,60*self.args["minutes"])
         self.listen_event(self.received_sensor_value, event = "knx_event", address = self.args["ga"])
     
     def received_sensor_value(self,event_name,data,kwargs):
-        self.cancel_timer(self.timer_handle)
-        self.timer_handle = self.run_in(self.remind_me,60*self.args["minutes"])
+        if self.timer_handle != None:
+            self.cancel_timer(self.timer_handle)
+        self.timer_handle = self.run_in(self.notify_me,60*self.args["minutes"])
 
     def notify_me(self, kwargs):
         self.log("Send warning, sensor did not send")
         self.fire_event("custom_notify", message="⚠️ KNX-Sensor {} hat nicht innerhalb {} Minuten gesendet ⚠️".format(self.args["sensor_name"],self.args["minutes"]), target="telegram_jo")
+        self.timer_handle = None
