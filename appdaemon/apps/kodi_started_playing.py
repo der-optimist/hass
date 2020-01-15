d302372be73ad3deb0bb962137c1559d2c2c9046
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-01-15 14:52:43 +0100

    Create kodi_started_playing.py

---
+++ appdaemon/apps/kodi_started_playing.py
@@ -0,0 +1,26 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+#
+# App sets an input_boolean to "on" when kodi starts playing
+# and resets it to "off" a few seconds later 
+# => switching off lights with auto-light app
+# But: only in the evening!
+#
+# Args:
+#
+
+class kodi_started_playing(hass.Hass):
+
+    def initialize(self):
+        self.bool_var = "input_boolean.kodi_just_started_playing"
+        self.active_time_entity = "binary_sensor.tv_zeit_abend"
+        self.listen_state(self.kodi_state_changed, "media_player.kodi")
+    
+    def sensor_state_changed(self, entity, attribute, old, new, kwargs):
+        if new == "playing" and old != new:
+            if self.get_state(self.active_time_entity) == "on":
+                self.turn_on(self.bool_var)
+                self.run_in(self.reset_bool_var,2)
+    
+    def reset_bool_var(self, kwargs):
+        self.turn_off(self.bool_var)
