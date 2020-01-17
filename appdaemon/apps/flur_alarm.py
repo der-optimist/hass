865a19a95672695141dfa99675a06a6d67c97fe9
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-01-17 08:57:39 +0100

    Update flur_alarm.py

--- appdaemon/apps/flur_alarm.py
+++ appdaemon/apps/flur_alarm.py
@@ -23,5 +23,21 @@ class flur_alarm(hass.Hass):
         #self.log(old)
         #self.log(new)
         if new == "on" and old != "on":
-            message="Achtung, Bewegung! ({})".format(entity.replace("binary_sensor.","").replace("_"," "))
+            if entity == "binary_sensor.pm_o_fl_flur":
+                readable_name = "Flur OG"
+            elif entity == "binary_sensor.pm_o_fl_treppe":
+                readable_name = "Treppe OG (oben)"
+            elif entity == "binary_sensor.pm_e_fl_treppe_og":
+                readable_name = "Treppe OG (unten)"
+            elif entity == "binary_sensor.pm_e_fl_flur":
+                readable_name = "Flur EG"
+            elif entity == "binary_sensor.pm_e_wf_flur":
+                readable_name = "Flur EG"
+            elif entity == "binary_sensor.pm_e_wf_garderobe":
+                readable_name = "Windfang Garderobe"
+            elif entity == "binary_sensor.pm_e_wf_haustur":
+                readable_name = "Windfang Haustür"
+            else:
+                readable_name = entity.replace("binary_sensor.","").replace("pm_","").replace("_"," ")
+            message="Bewegung! => {}".format(readable_name)
             self.fire_event("custom_notify", message=message, target=self.target)
