be983b9e508747da3620e8358154998b294bbcb4
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-01-24 14:21:37 +0100

    Update locations.py

--- appdaemon/apps/locations.py
+++ appdaemon/apps/locations.py
@@ -14,31 +14,22 @@ class locations(hass.Hass):
     def initialize(self):
         self.sensor_location_jo = "sensor.location_jo" # where to show the information
         self.input_location_jo_roh = "input_text.location_jo_roh" # where tasker sends the value to
+        self.sensor_time_at_work = "sensor.jo_at_work"
         self.name_jo = self.args["name_jo"]
         # listen state
         self.listen_state(self.location_changed, self.input_location_jo_roh)
         # show the current location immediately after restart
         current_value = self.get_state(self.input_location_jo_roh)
         self.process_location(current_value, "restart")
-        # is jo at work?
-        if current_value == "Arbeit":
-            self.set_state("binary_sensor.jo_at_work_appdaemon", "on")
-        else:
-            self.set_state("binary_sensor.jo_at_work_appdaemon", "off")
         
     def location_changed(self, entity, attribute, old, new, kwargs):
         self.process_location(new, old)
         
     def process_location(self, location, old_location):
-        # at work or not?
-        if location == "Arbeit":
-            self.set_state("binary_sensor.jo_at_work_appdaemon", "on")
-        else:
-            self.set_state("binary_sensor.jo_at_work_appdaemon", "off")
         # really process the location now
         if location == "Arbeit":
             self.set_state(self.sensor_location_jo, state = "Bei der Arbeit", attributes={"entity_picture":"/local/icons/locations/manager.svg", "friendly_name": self.name_jo})
-            time_at_work = self.get_state("sensor.jo_at_work")
+            time_at_work = self.get_state(self.sensor_time_at_work)
             self.log("Time at work: {}".format(time_at_work))
             if time_at_work == "0.0":
                 self.fire_event("custom_notify", message="Deiner lieben Frau schreiben!", target="telegram_jo")
