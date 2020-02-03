d5afc5ca210c6584a86f8fa88826d0276f2a87a9
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-02-03 09:19:20 +0100

    Update camera_motion_alarm.py

--- appdaemon/apps/camera_motion_alarm.py
+++ appdaemon/apps/camera_motion_alarm.py
@@ -9,10 +9,10 @@ import appdaemon.plugins.hass.hassapi as hass
 class camera_motion_alarm(hass.Hass):
 
     def initialize(self):
-        self.listen_event(self.folder_watcher_event, "folder_watcher")
+        self.listen_event(self.folder_watcher_event, "folder_watcher", event_type = 'created')
         self.call_service('notify/telegram_jo',
-                  message="Bewegung",
-                  data= {'photo': {'file': '/share/abstellraum/20200203/images/A20020308260010.jpg', 'caption': 'Abstellraum'}})
+                  message="Test",
+                  data= {'photo': {'file': '/share/abstellraum/20200203/images/A20020308260010.jpg', 'caption': 'Test'}})
     
     def folder_watcher_event(self, event_name, data, kwargs):
         self.log(event_name)
@@ -21,4 +21,4 @@ class camera_motion_alarm(hass.Hass):
         self.log(image_path)
         self.call_service('notify/telegram_jo',
                           message="Bewegung",
-                          data= {'photo': {'file': image_path}})
+                          data= {'photo': {'file': image_path, 'caption': 'Abstellraum'}})
