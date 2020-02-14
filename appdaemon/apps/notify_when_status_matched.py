f1d975536d4ebd754336b034bc341c19d87748e2
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-02-14 14:44:33 +0100

    Update notify_when_status_matched.py

--- appdaemon/apps/notify_when_status_matched.py
+++ appdaemon/apps/notify_when_status_matched.py
@@ -14,11 +14,12 @@ import appdaemon.plugins.hass.hassapi as hass
 class notify_when_status_matched(hass.Hass):
 
     def initialize(self):
-        self.log(self.args["previous_status"])
         if self.args["previous_status"] == None
+            self.log("prev_state ist None")
             self.listen_state(self.sensor_state_changed, self.args["entity"], new = self.args["status_to_match"])
         else:
             self.listen_state(self.sensor_state_changed, self.args["entity"], new = self.args["status_to_match"], old = self.args["previous_status"])
+            self.log("prev_state ist {}".format(self.args["previous_status"]))
     
     def sensor_state_changed(self, entity, attribute, old, new, kwargs):
         if new != old:
