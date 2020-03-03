c287c91a5f36c071a1c9fd081de1598969809f98
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-03-03 14:31:48 +0100

    Update cover_tilt_pos_when_state_matched.py

--- appdaemon/apps/cover_tilt_pos_when_state_matched.py
+++ appdaemon/apps/cover_tilt_pos_when_state_matched.py
@@ -19,13 +19,13 @@ class cover_tilt_pos_when_state_matched(hass.Hass):
         self.listen_state(self.state_matched, self.args["match_entity"], old = self.args["match_state_old"], new = self.args["match_state_new"])
 
     def state_matched(self, entity, attributes, old, new, kwargs):
-        if self.args["match_entity"] == "tilt":
+        if self.args["mode"] == "tilt":
             if float(self.get_state(self.args["cover_entity"], attribute="current_position")) > 0:
                 self.call_service("cover/set_cover_tilt_position", entity_id = self.args["cover_entity"], tilt_position = self.args["target_tilt"])
                 self.log("Habe Jalousie {} auf Winkel {} gestellt".format(self.args["cover_entity"],self.args["target_tilt"]))
             else:
                 self.log("Sollte Jalousie {} auf Winkel {} stellen, sie war aber oben. Habe nichts gemacht".format(self.args["cover_entity"],self.args["target_tilt"]))
-        elif self.args["match_entity"] == "tilt_pos":
+        elif self.args["mode"] == "tilt_pos":
             self.call_service("cover/set_cover_position", entity_id = self.args["cover_entity"], position = self.args["target_position"])
             self.call_service("cover/set_cover_tilt_position", entity_id = self.args["cover_entity"], tilt_position = self.args["target_tilt"])
         else:
