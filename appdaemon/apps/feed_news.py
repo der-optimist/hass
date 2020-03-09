561e0fc1dfcecf2a3b13d1197f16a18e041459a5
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-03-09 12:59:40 +0100

    Update feed_news.py

--- appdaemon/apps/feed_news.py
+++ appdaemon/apps/feed_news.py
@@ -1,11 +1,11 @@
 import appdaemon.plugins.hass.hassapi as hass
 import xml.etree.ElementTree as ET
 import requests, io
-import json
 import datetime
 
 #
 # What it does:
+# Check for new news video and write the new link to the html "player"
 #
 # What args it needs:
 # 
@@ -17,8 +17,8 @@ class feed_news(hass.Hass):
         self.url_tagesschau_100s = "https://www.tagesschau.de/export/video-podcast/webxl/tagesschau-in-100-sekunden_https/"
         self.path_player_html = "/config/www/tagesschau/player_appdaemon.html"
         self.url_video = None
-        #self.run_every(self.load_tagesschau_100s, datetime.datetime.now() + datetime.timedelta(seconds=3), 5 * 60) # update every 5 minutes
-        self.load_tagesschau_100s(None)
+        self.run_every(self.load_tagesschau_100s, datetime.datetime.now() + datetime.timedelta(seconds=13), 10 * 60) # update every 10 minutes
+        #self.load_tagesschau_100s(None)
 
     def load_tagesschau_100s(self, kwargs):
         try:
@@ -33,19 +33,22 @@ class feed_news(hass.Hass):
             tree = ET.parse(xml)
             root = tree.getroot()
             link = root.findall('channel')[0].findall('item')[0].findall('enclosure')[0].get("url")
-            self.log(link)
-            self.url_video = link
-            self.write_player_html()
+            if self.url_video != link:
+                self.log("Jhu, neue Tagesschau!")
+                self.url_video = link
+                self.write_player_html()
+            else:
+                self.log("alte Tagesschau")
 
     def write_player_html(self):
         f = open(self.path_player_html, "w")
-        f.write('<!DOCTYPE html>')
-        f.write('<html>')
-        f.write('<body>')
-        f.write('<video width="95%" autoplay controls>')
-        f.write('  <source src="{}" type="video/mp4">'.format(self.url_video))
-        f.write('  Your browser does not support the video tag.')
-        f.write('</video>')
-        f.write('</body>')
-        f.write('</html>')
+        f.write('<!DOCTYPE html>\n')
+        f.write('<html>\n')
+        f.write('<body>\n')
+        f.write('<video width="95%" autoplay controls>\n')
+        f.write('  <source src="{}" type="video/mp4">\n'.format(self.url_video))
+        f.write('  Your browser does not support the video tag.\n')
+        f.write('</video>\n')
+        f.write('</body>\n')
+        f.write('</html>\n')
         f.close()
