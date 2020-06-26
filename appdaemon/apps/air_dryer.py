e05e56bfa0ca62ac2bea30ca25ce8899c7c42b2a
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-06-26 15:35:27 +0200

    Update air_dryer.py

--- appdaemon/apps/air_dryer.py
+++ appdaemon/apps/air_dryer.py
@@ -24,7 +24,9 @@ class air_dryer(hass.Hass):
         self.time_2_hours = int(self.args["time_2_hours"])
         self.input_number_timer_special_humidity = self.args["input_number_timer_special_humidity"]
         
-        self.listen_state(self.timer_state_change, self.input_number_timer_special_humidity)
+        self.listen_state(self.timer_state_changed, self.input_number_timer_special_humidity)
+        self.listen_state(self.humidity_state_changed, self.humidity_sensor)
+        self.listen_state(self.electrical_measurement_state_changed, self.energy_measurement_sensor)
         self.listen_event(self.button_time_1, "zha_event", device_ieee = self.zha_device_ieee, command = "zha_device_command_time_1")
         self.listen_event(self.button_time_2, "zha_event", device_ieee = self.zha_device_ieee, command = "zha_device_command_time_2")
         
@@ -32,16 +34,19 @@ class air_dryer(hass.Hass):
         self.time_internal_state = 0
         if not float(self.get_state(self.input_number_timer_special_humidity)) == float(0):
             self.log("Timer nicht Null als ich gestartet wurde, werde jetzt weiter runter zaehlen")
+            self.special_mode = True
             self.stunde_abgelaufen(None)
         else:
             self.log("Timer ist Null als ich gestartet wurde, alles ruhig hier...")
+            self.special_mode = False
     
-    def timer_state_change(self, entity, attributes, old, new, kwargs):
+    def timer_state_changed(self, entity, attributes, old, new, kwargs):
         self.log("State Change in Humidity Timer erkannt: {}".format(new))
         if round(float(new)) == 0:
             if self.time_internal_state == 0:
                 self.log("Timer abgelaufen, werde jetzt auf normale Luftfeuchtigkeit umschalten")
                 self.timer_handle = None
+                self.special_mode = False
             else:
                 self.log("Timer wurde wohl manuell auf 0 gestellt, breche den Timer ab")
                 self.time_internal_state = 0
@@ -49,6 +54,7 @@ class air_dryer(hass.Hass):
                     self.cancel_timer(self.timer_handle)
                     self.log("Timer wurde abgebrochen")
                     self.timer_handle = None
+                    self.special_mode = False
         else:
             if round(float(new)) == self.time_internal_state:
                 self.log("Event wurde wohl durch mich selber ausgelöst weil eine Stunde um ist, werde eine neue Stunde timen...")
@@ -61,6 +67,14 @@ class air_dryer(hass.Hass):
                 self.time_internal_state = round(float(new))
                 self.timer_handle = self.run_in(self.stunde_abgelaufen,3600)
 
+    def humidity_state_changed(self, entity, attributes, old, new, kwargs):
+        if self.special_mode:
+            pass
+            
+    def electrical_measurement_state_changed(self, entity, attributes, old, new, kwargs):
+        pass
+        # Tank voll?
+        
     def button_time_1(self,event_name,data,kwargs):
         self.time_internal_state = self.time_1_hours
         self.set_value(self.input_number_timer_special_humidity, self.time_1_hours)
