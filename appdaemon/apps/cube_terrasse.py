95fff45bb71158511646dcdd50166a8d089f31ef
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-06-29 11:02:33 +0200

    Create cube_terrasse.py

---
+++ appdaemon/apps/cube_terrasse.py
@@ -0,0 +1,75 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+#
+# App to turn lights on/off outside with an aqara cube
+#
+# Args: see initialize()
+# 
+
+class cube_terrasse(hass.Hass):
+
+    def initialize(self):
+        # Args
+        self.zha_device_ieee = self.args["zha_device_ieee"]
+        
+        self.active_side = None
+        self.listen_event(self.cube_flip, "zha_event", device_ieee = self.zha_device_ieee, command = "flip")
+        self.listen_event(self.cube_shake, "zha_event", device_ieee = self.zha_device_ieee, command = "shake")
+        self.listen_event(self.cube_knock, "zha_event", device_ieee = self.zha_device_ieee, command = "knock")
+        self.listen_event(self.cube_rotate_right, "zha_event", device_ieee = self.zha_device_ieee, command = "rotate_right")
+        self.listen_event(self.cube_rotate_left, "zha_event", device_ieee = self.zha_device_ieee, command = "rotate_left")
+        
+        
+    def cube_flip(self,event_name,data,kwargs):
+        self.active_side = data["args"]["activated_face"]
+        self.log("Active Side: {}".format(self.active_side))
+    
+    def cube_shake(self,event_name,data,kwargs):
+        if self.get_state("light.wandwurfel_suden") == "on":
+            wandwurfel_suden = True
+        else:
+            wandwurfel_suden = False
+        if self.get_state("light.wandwurfel_westen") == "on":
+            wandwurfel_westen = True
+        else:
+            wandwurfel_westen = False
+            
+        if wandwurfel_suden or wandwurfel_westen:
+            self.turn_off("light.wandwurfel_suden")
+            self.turn_off("light.wandwurfel_westen")
+            self.log("Beide oder ein Wandwürfel waren an, habe sie aus gemacht")
+        else:
+            self.turn_on("light.wandwurfel_suden")
+            self.turn_on("light.wandwurfel_westen")
+            self.log("Beide Wandwürfel waren aus, habe sie an gemacht")
+
+    def cube_knock(self,event_name,data,kwargs):
+        self.active_side = data["args"]["activated_face"]
+        self.log("Active Side: {}".format(self.active_side))
+        if self.active_side == 5:
+            self.toggle("light.wandwurfel_suden")
+            self.log("toggled Würfel Süd")
+        elif self.active_side == 1:
+            self.toggle("light.wandwurfel_westen")
+            self.log("toggled Würfel Westen")
+    
+    def cube_rotate_right(self,event_name,data,kwargs):
+        if self.active_side == 5:
+            self.turn_on("light.wandwurfel_suden")
+            self.log("Würfel Süd an gemach wegen Rotate Right")
+        elif self.active_side == 1:
+            self.turn_on("light.wandwurfel_westen")
+            self.log("Würfel Westen an gemach wegen Rotate Right")
+        else:
+            self.log("Rotate Right erkannt, aber active side ist {}".format(self.active_side))
+    
+    def cube_rotate_left(self,event_name,data,kwargs):
+        if self.active_side == 5:
+            self.turn_off("light.wandwurfel_suden")
+            self.log("Würfel Süd aus gemach wegen Rotate Left")
+        elif self.active_side == 1:
+            self.turn_off("light.wandwurfel_westen")
+            self.log("Würfel Westen aus gemach wegen Rotate Left")
+        else:
+            self.log("Rotate Left erkannt, aber active side ist {}".format(self.active_side))
+            
