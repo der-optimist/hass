69802bd53f55c90a77731f9330ad0a76032029eb
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-07-01 14:12:58 +0200

    Update error_handling.py

--- appdaemon/apps/error_handling.py
+++ appdaemon/apps/error_handling.py
@@ -14,12 +14,12 @@ class error_handling(hass.Hass):
 
     def error_event(self,event_name,data,kwargs):
         self.log("Error erkannt. Source ist {}".format(data["source"]))
-        message = "Home Assistant Error"\
-                  "source: "\
-                  "{}"\
-                  "message: "\
-                  "{}"\
-                  "exception:"\
+        message = "Home Assistant ERROR\n"\
+                  "source:\n"\
+                  "{}\n"\
+                  "message:\n"\
+                  "{}\n"\
+                  "exception:\n"\
                   "{}".format(data["source"],data["message"],data["exception"])
         self.fire_event("custom_notify", message=message, target="telegram_jo")
         if data["source"] == "homeassistant.core" and data["message"].startswith("Error doing job: Task exception was never retrieved"):
@@ -27,11 +27,11 @@ class error_handling(hass.Hass):
 
     def warning_event(self,event_name,data,kwargs):
         self.log("Warning erkannt. Source ist {}".format(data["source"]))
-        message = "Home Assistant Warning"\
-                  "source: "\
-                  "{}"\
-                  "message: "\
-                  "{}"\
-                  "exception:"\
+        message = "Home Assistant WARNING\n"\
+                  "source:\n"\
+                  "{}\n"\
+                  "message:\n"\
+                  "{}\n"\
+                  "exception:\n"\
                   "{}".format(data["source"],data["message"],data["exception"])
         self.fire_event("custom_notify", message=message, target="telegram_jo")
