bf90a77907bb368def3286a45d24ecf4a0869502
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-07-01 13:11:09 +0200

    Update error_handling.py

--- appdaemon/apps/error_handling.py
+++ appdaemon/apps/error_handling.py
@@ -10,6 +10,7 @@ class error_handling(hass.Hass):
 
     def initialize(self):
         self.listen_event(self.error_event, "system_log_event", level = "ERROR")
+        self.listen_event(self.warning_event, "system_log_event", level = "WARNING")
 
     def error_event(self,event_name,data,kwargs):
         self.log("Error erkannt. Source ist {}".format(data["source"]))
@@ -23,3 +24,14 @@ class error_handling(hass.Hass):
         self.fire_event("custom_notify", message=message, target="telegram_jo")
         if data["source"] == "homeassistant.core" and data["message"].startswith("Error doing job: Task exception was never retrieved"):
             self.fire_event("custom_notify", message="KNX-Fehler erkannt", target="telegram_jo")
+
+    def warning_event(self,event_name,data,kwargs):
+        self.log("Warning erkannt. Source ist {}".format(data["source"]))
+        message = "Home Assistant Warning"\
+                  "source: "\
+                  "{}"\
+                  "message: "\
+                  "{}"\
+                  "exception:"\
+                  "{}".format(data["source"],data["message"],data["exception"])
+        self.fire_event("custom_notify", message=message, target="telegram_jo")
