c729092636bdf7d178c05546bf6240608a881285
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-07-21 12:24:11 +0200

    Update save_reminders.py

--- appdaemon/apps/save_reminders.py
+++ appdaemon/apps/save_reminders.py
@@ -13,11 +13,13 @@ class save_reminders(hass.Hass):
         self.set_textvalue("input_text.saved_reminder_001", "wiederhergestellteerinnerung::Test: Tu was::/local/icons/reminders/exclamation_mark_blink.svg")
         time.sleep(1)
         text = self.get_state("input_text.saved_reminder_001")
-        text = "test"
-        switch_name = "switch.reminder_" + text.split("::")[0]
-        friendly_name = text.split("::")[1]
-        icon = text.split("::")[2]
-        self.set_state(switch_name, state = "on", attributes={"entity_picture":icon, "friendly_name": friendly_name})
+        if len(text.split("::") == 3):
+            switch_name = "switch.reminder_" + text.split("::")[0]
+            friendly_name = text.split("::")[1]
+            icon = text.split("::")[2]
+            self.set_state(switch_name, state = "on", attributes={"entity_picture":icon, "friendly_name": friendly_name})
+        else:
+            self.log("input_text sollte 2 mal :: enthalten")
         #self.listen_state(self.illuminance_changed, self.args["illuminance_sensor"])
 
     def illuminance_changed(self, entity, attributes, old, new, kwargs):
