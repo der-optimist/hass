a71cde466e0b6c63b53927b741a9892572f7636e
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-07-21 10:11:29 +0200

    Update save_reminders.py

--- appdaemon/apps/save_reminders.py
+++ appdaemon/apps/save_reminders.py
@@ -9,7 +9,7 @@ import appdaemon.plugins.hass.hassapi as hass
 class save_reminders(hass.Hass):
 
     def initialize(self):
-        self.set_textvalue("input_text.saved_reminder_001", "reminder_name", icon="/local/icons/reminders/exclamation_mark_blink.svg")
+        self.set_textvalue("input_text.saved_reminder_001", "entkalkungsanlagesalzkontrollieren::/local/icons/reminders/exclamation_mark_blink.svg::Entkalkungsanlage: Salz kontrollieren")
         #self.listen_state(self.illuminance_changed, self.args["illuminance_sensor"])
 
     def illuminance_changed(self, entity, attributes, old, new, kwargs):
