d02bc181ae084cc4a04b25f8d4fa696062a5be1e
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-07-21 12:20:20 +0200

    Update save_reminders.py

--- appdaemon/apps/save_reminders.py
+++ appdaemon/apps/save_reminders.py
@@ -1,4 +1,5 @@
 import appdaemon.plugins.hass.hassapi as hass
+import time
 
 #
 # make custom reminders survive a restart
@@ -9,7 +10,14 @@ import appdaemon.plugins.hass.hassapi as hass
 class save_reminders(hass.Hass):
 
     def initialize(self):
-        self.set_textvalue("input_text.saved_reminder_001", "entkalkungsanlagesalzkontrollieren::/local/icons/reminders/exclamation_mark_blink.svg::Entkalkungsanlage: Salz kontrollieren")
+        self.set_textvalue("input_text.saved_reminder_001", "wiederhergestellteerinnerung::Test: Tu was::/local/icons/reminders/exclamation_mark_blink.svg")
+        time.sleep(1)
+        text = self.get_state("input_text.saved_reminder_001")
+        text = "test"
+        switch_name = "switch.reminder_" + text.split("::")[0]
+        friendly_name = text.split("::")[1]
+        icon = text.split("::")[2]
+        self.set_state(switch_name, state = "on", attributes={"entity_picture":icon, "friendly_name": friendly_name})
         #self.listen_state(self.illuminance_changed, self.args["illuminance_sensor"])
 
     def illuminance_changed(self, entity, attributes, old, new, kwargs):
