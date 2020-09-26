137750b35356b7239870c4e2acfa54de2d7719ca
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-09-26 15:03:39 +0200

    Update sync_scene_and_night_entities.py

--- appdaemon/apps/sync_scene_and_night_entities.py
+++ appdaemon/apps/sync_scene_and_night_entities.py
@@ -25,10 +25,10 @@ class sync_scene_and_night_entities(hass.Hass):
         self.log(data)
         if data["data"] == [0]:
             self.log("La geht ins Bett")
-            self.turn_on("light.innr_rb_245_0b674403_level_on_off", brightness=255)
+            self.turn_on("light.innr_e14_ww_stehlampe_lara_level_on_off", brightness=255)
         elif data["data"] == [1]:
             self.log("Le geht ins Bett")
-            self.turn_on("light.innr_rb_248_t_2192ebfe_level_light_color_on_off", brightness=255)
+            self.turn_on("light.innr_e14_tw_stehlampe_lea_level_light_color_on_off", brightness=255)
         #elif data["data"] == [3]:
         elif data["data"] == [3]:
             self.log("Fernseh-Szene")
