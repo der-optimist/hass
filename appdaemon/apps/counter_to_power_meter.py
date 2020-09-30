6ac3969018049d9803c0b873a0b90261358552b8
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-09-30 10:09:39 +0200

    Update counter_to_power_meter.py

--- appdaemon/apps/counter_to_power_meter.py
+++ appdaemon/apps/counter_to_power_meter.py
@@ -8,6 +8,7 @@ import datetime
 #  - Calculate power in W (from previous to current event)
 #
 # Args:
+# offset_kwh = Offset for electricity sensor
 # knx_counter = "sensor.stromzahler_xyz_rohdaten"
 # ha_electricity_sensor_name = "sensor.stromzahler_xyz"
 # ha_electricity_sensor_friendly_name = "Stromzähler XYZ"
@@ -29,13 +30,14 @@ class counter_to_power_meter(hass.Hass):
         self.time_of_last_event = None
         self.value_of_last_event = None
         self.handle_ramp_down_timer = None
+        self.offset_kwh = self.args["offset_kwh"]
         try:
             current_electricity = round(float(self.get_state(self.args["ha_electricity_sensor_name"])))
             self.log("current electricity ist {}".format(current_electricity))
         except Exception as e:
             current_electricity = 0
             self.log("error while loading current electrcity. error is {}".format(e))
-        self.set_state(self.args["ha_electricity_sensor_name"], state = current_electricity, attributes={"icon":"mdi:counter", "friendly_name": self.args["ha_electricity_sensor_friendly_name"], "unit_of_measurement": "kWh"})
+        self.set_state(self.args["ha_electricity_sensor_name"], state = (current_electricity + self.offset_kwh), attributes={"icon":"mdi:counter", "friendly_name": self.args["ha_electricity_sensor_friendly_name"], "unit_of_measurement": "kWh"})
         self.set_state(self.args["ha_power_sensor_name"], state = 0.0, attributes={"icon":"mdi:speedometer", "friendly_name": self.args["ha_power_sensor_friendly_name"], "unit_of_measurement": "W", "device_class": "power"})
         
     def counter_changed(self, entity, attribute, old, new, kwargs):
@@ -45,7 +47,7 @@ class counter_to_power_meter(hass.Hass):
             self.cancel_timer(self.handle_ramp_down_timer)
         current_time = datetime.datetime.now() # for most accurate value, capture current time first
         # Update electricity meter sensor
-        new_electricity_value = float(new) * self.args["energy_per_pulse"]
+        new_electricity_value = (float(new) * self.args["energy_per_pulse"]) + self.offset_kwh
         attributes = self.get_state(self.args["ha_electricity_sensor_name"], attribute="all")["attributes"]
         self.set_state(self.args["ha_electricity_sensor_name"], state = round(new_electricity_value), attributes=attributes)
         # calculate power
@@ -58,7 +60,7 @@ class counter_to_power_meter(hass.Hass):
                 time_delta_seconds = (current_time - self.time_of_last_event).total_seconds()
                 electricity_delta_Ws = (float(new) - self.value_of_last_event) * self.args["energy_per_pulse"] * 3600 * 1000
                 current_power = electricity_delta_Ws / time_delta_seconds
-                if current_power > (float(self.args["phases"]) * self.max_plausible_watt_per_phase):
+                if (current_power > (float(self.args["phases"]) * self.max_plausible_watt_per_phase)) or current_power < 0:
                     self.log("Unplausibler Wert für Leistung: {} Watt - werde ihn ignorieren".format(round(current_power, 1)))
                 else:
                     attributes = self.get_state(self.args["ha_power_sensor_name"], attribute="all")["attributes"]
@@ -71,7 +73,6 @@ class counter_to_power_meter(hass.Hass):
 
     def ramp_down(self, kwargs):
         current_time = datetime.datetime.now() # for most accurate value, capture current time first
-#        new_virtual_electricity_value = (self.value_of_last_event + self.args["knx_sending_every"]) * self.args["energy_per_pulse"]
         time_delta_seconds = (current_time - self.time_of_last_event).total_seconds()
         electricity_delta_Ws = self.args["knx_sending_every"] * self.args["energy_per_pulse"] * 3600 * 1000
         current_power = electricity_delta_Ws / time_delta_seconds
