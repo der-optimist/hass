82ffa0069e289299c0ad0d0f2ea17306b248a3b7
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-10-01 13:58:28 +0200

    Update energy_consumption_from_power_logs.py

--- appdaemon/apps/energy_consumption_from_power_logs.py
+++ appdaemon/apps/energy_consumption_from_power_logs.py
@@ -40,28 +40,31 @@ class energy_consumption_from_power_logs(hass.Hass):
         ts_end_local_ns = ts_end_local * 1e9 + 999
         utc_offset_timestamp = datetime.datetime.now().timestamp() - datetime.datetime.utcnow().timestamp()
         query = 'SELECT "{}" FROM "homeassistant_permanent"."autogen"."{}" WHERE time >= {} AND time <= {} ORDER BY time DESC'.format(self.db_field, self.db_measurement, int(ts_start_local_ns_plus_buffer), int(ts_end_local_ns))
-        self.log(query)
+        #self.log(query)
         result_points = self.client.query(query).get_points()
         consumption_Ws = 0.0
         past_timestep = ts_end_local
         start_time_reached = False
         for point in result_points:
-            self.log(point)
+            #self.log(point)
             timestamp_local = datetime.datetime.strptime(point["time"], '%Y-%m-%dT%H:%M:%S.%fZ').timestamp() + utc_offset_timestamp
-            self.log("timestamp local: {}".format(timestamp_local))
+            #self.log("timestamp local: {}".format(timestamp_local))
             if timestamp_local < ts_start_local:
                 timestamp_local = ts_start_local
                 start_time_reached = True
-            time_delta_s = timestamp_local - past_timestep
-            self.log("timedelta: {} s".format(time_delta_s))
+            time_delta_s = past_timestep - timestamp_local
+            #self.log("timedelta: {} s".format(time_delta_s))
             energy_Ws = point[self.db_field] * time_delta_s
-            self.log("energy: {} Ws".format(energy_Ws))
+            #self.log("energy: {} Ws".format(energy_Ws))
             consumption_Ws = consumption_Ws + energy_Ws
             past_timestep = timestamp_local
             if start_time_reached:
                 break
         consumption_kWh = consumption_Ws / 3600000
         self.log("Verbrauch: {} Ws bzw. {} kWh".format(consumption_Ws, consumption_kWh))
+        # save to db
+        ts_save_local_ns = datetime.datetime.strptime(self.date + 'T23:59:59.0', '%Y-%m-%dT%H:%M:%S.%f').timestamp() * 1e9
+        self.client.write_points([{"measurement":"energy_consumption_test","fields":{"lueftungsanlage":consumption_kWh},"time":ts_save_local_ns}])
 #            if newest_value == None:
 #                newest_value = point[self.db_field]
 #                current_time = datetime.datetime.utcnow()
