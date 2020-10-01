a11b478407a7e3399fc05e9e4f107429b6f1efe4
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-10-01 11:24:14 +0200

    Update energy_consumption_from_power_logs.py

--- appdaemon/apps/energy_consumption_from_power_logs.py
+++ appdaemon/apps/energy_consumption_from_power_logs.py
@@ -35,26 +35,25 @@ class energy_consumption_from_power_logs(hass.Hass):
     def calculate_energy_consumption(self, kwargs):
         ts_start = datetime.datetime.strptime(self.date + 'T00:00:00.0', '%Y-%m-%dT%H:%M:%S.%f').timestamp() * 1e9
         ts_end = datetime.datetime.strptime(self.date + 'T23:59:59.999999', '%Y-%m-%dT%H:%M:%S.%f').timestamp() * 1e9 + 999
-        query = 'SELECT "{}" FROM "homeassistant_permanent"."autogen"."{}" WHERE time >= {} AND time <= {} ORDER BY time DESC'.format(self.db_field, self.db_measurement, int(ts_start), (ts_end))
+        query = 'SELECT "{}" FROM "homeassistant_permanent"."autogen"."{}" WHERE time >= {} AND time <= {} ORDER BY time DESC'.format(self.db_field, self.db_measurement, int(ts_start), int(ts_end))
         self.log(query)
         result_points = self.client.query(query).get_points()
         self.log(result_points)
-        return
         newest_value = None
         current_time = None
         for point in result_points:
             self.log(point)
-            if newest_value == None:
-                newest_value = point[self.db_field]
-                current_time = datetime.datetime.utcnow()
+#            if newest_value == None:
+#                newest_value = point[self.db_field]
+#                current_time = datetime.datetime.utcnow()
                 #self.log(current_time.strftime("%Y-%m-%d %H:%M:%S"))
-            else:
-                delta_value = point[self.db_field] - newest_value
-                delta_time = datetime.datetime.strptime(point["time"][:-4], '%Y-%m-%dT%H:%M:%S.%f') - current_time
+#            else:
+#                delta_value = point[self.db_field] - newest_value
+#                delta_time = datetime.datetime.strptime(point["time"][:-4], '%Y-%m-%dT%H:%M:%S.%f') - current_time
                 #self.log(datetime.datetime.strptime(point["time"][:-4], '%Y-%m-%dT%H:%M:%S.%f').strftime("%Y-%m-%d %H:%M:%S"))
-                delta_time_seconds = delta_time.total_seconds()
-                derivative = delta_value / (delta_time_seconds / 3600)
-                der_list.append(derivative)
+#                delta_time_seconds = delta_time.total_seconds()
+#                derivative = delta_value / (delta_time_seconds / 3600)
+#                der_list.append(derivative)
 
             
 
