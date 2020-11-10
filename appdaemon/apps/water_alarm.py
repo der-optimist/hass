7fd9a544d9c1ade91ea69d3b4c087f99100b0626
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-11-10 08:55:18 +0100

    Update and rename water_alarm_washing_machine.py to water_alarm.py

--- appdaemon/apps/water_alarm.py
+++ appdaemon/apps/water_alarm.py
@@ -9,7 +9,7 @@ import requests
 # none
 #
 
-class water_alarm_washing_machine(hass.Hass):
+class water_alarm(hass.Hass):
 
     def initialize(self):
         self.url = "http://192.168.178.42:2971/api/command"
@@ -17,8 +17,9 @@ class water_alarm_washing_machine(hass.Hass):
         
     def initialize_delayed(self, kwargs):
         self.listen_state(self.eimer_hebeanlage_voll, "binary_sensor.eimer_hebeanlage_voll", new = "on")
-        self.listen_state(self.wasser_boden, "binary_sensor.wasser_boden_bei_hebeanlage", new = "on")
+        self.listen_state(self.wasser_boden_hebeanlage, "binary_sensor.wasser_boden_bei_hebeanlage", new = "on")
         self.listen_state(self.sicherung_hebeanlage_raus, "binary_sensor.sicherung_keller_sd_rausgeflogen", new = "on")
+        self.listen_state(self.wasser_boden_spuelmaschine, "binary_sensor.wasser_unter_spule", new = "on")
         self.listen_event(self.button_wm_strom_an, "zha_event", device_ieee = "00:15:8d:00:04:0b:11:2f", command = "right_single")
     
     def eimer_hebeanlage_voll(self, entity, attribute, old, new, kwargs):
@@ -27,6 +28,7 @@ class water_alarm_washing_machine(hass.Hass):
             message = "Eimer Hebeanlage voll - habe die Waschmaschinen-Steckdose ausgeschalten!"
             self.fire_event("custom_notify", message=message, target="telegram_jo")
             self.fire_event("custom_notify", message=message, target="telegram_ma")
+            # alarm message via separate notify_when_status_matched app
             try:
                 requests.post(self.url, json={"speak":"Alarm! Eimer Hebeanlage ist voll"}, timeout=5)
             except:
@@ -34,12 +36,13 @@ class water_alarm_washing_machine(hass.Hass):
             fc = FritzCall(address=self.args["fritz_address"], password=self.args["fritz_pw"])
             fc.dial(self.args["phone_jo_handy"])
                             
-    def wasser_boden(self, entity, attribute, old, new, kwargs):
+    def wasser_boden_hebeanlage(self, entity, attribute, old, new, kwargs):
         if new != old:
             self.turn_off("switch.waschmaschine")
             message = "Wasser auf dem Boden bei der Hebeanlage - habe die Waschmaschinen-Steckdose ausgeschalten!"
             self.fire_event("custom_notify", message=message, target="telegram_jo")
             self.fire_event("custom_notify", message=message, target="telegram_ma")
+            # alarm message via separate notify_when_status_matched app
             try:
                 requests.post(self.url, json={"speak":"Alarm! Wasser auf dem Boden bei der Hebeanlage"}, timeout=5)
             except:
@@ -53,13 +56,28 @@ class water_alarm_washing_machine(hass.Hass):
             message = "Sicherung Keller-Steckdosen (Hebeanlage!) rausgeflogen - habe die Waschmaschinen-Steckdose ausgeschalten!"
             self.fire_event("custom_notify", message=message, target="telegram_jo")
             self.fire_event("custom_notify", message=message, target="telegram_ma")
+            # alarm message via separate notify_when_status_matched app
             try:
                 requests.post(self.url, json={"speak":"Achtung! Hebeanlage hat keinen Strom"}, timeout=5)
             except:
             	pass
             fc = FritzCall(address=self.args["fritz_address"], password=self.args["fritz_pw"])
             fc.dial(self.args["phone_jo_handy"])
-            
+
+    def wasser_boden_spuelmaschine(self, entity, attribute, old, new, kwargs):
+        if new != old:
+            #self.turn_off("switch.waschmaschine")
+            message = "Wasser auf dem Boden unter der Spüle oder Spülmaschine!"
+            self.fire_event("custom_notify", message=message, target="telegram_jo")
+            self.fire_event("custom_notify", message=message, target="telegram_ma")
+            # alarm message via separate notify_when_status_matched app
+            try:
+                requests.post(self.url, json={"speak":"Alarm! Wasser auf dem Boden unter der Spüle oder Spülmaschine"}, timeout=5)
+            except:
+            	pass
+            fc = FritzCall(address=self.args["fritz_address"], password=self.args["fritz_pw"])
+            fc.dial(self.args["phone_jo_handy"])
+
     def button_wm_strom_an(self,event_name,data,kwargs):
         self.turn_on("switch.waschmaschine")
         message = "Waschmaschine hat (wieder) Strom"
