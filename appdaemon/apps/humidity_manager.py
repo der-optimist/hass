6fc7e82755ece017d654996f31f636750f0bbc23
der-optimist <45357614+der-optimist@users.noreply.github.com>
2020-11-27 14:35:41 +0100

    Update humidity_manager.py

--- appdaemon/apps/humidity_manager.py
+++ appdaemon/apps/humidity_manager.py
@@ -35,13 +35,13 @@ class humidity_manager(hass.Hass):
     def update_notification(self, entity, attributes, old, new, kwargs):
         try:
             humidity_inside = float(self.get_state(self.sensor_humidity_inside))
-            self.log("Luftfeuchtigkeit innen: {} %".format(round(humidity_inside,1)))
+            #self.log("Luftfeuchtigkeit innen: {} %".format(round(humidity_inside,1)))
             temp_inside = float(self.get_state(self.sensor_temp_inside))
-            self.log("Temperatur innen: {} C".format(round(temp_inside,1)))
+            #self.log("Temperatur innen: {} C".format(round(temp_inside,1)))
             humidity_outside = float(self.get_state(self.sensor_humidity_outside))
-            self.log("Luftfeuchtigkeit aussen: {} %".format(round(humidity_outside,1)))
+            #self.log("Luftfeuchtigkeit aussen: {} %".format(round(humidity_outside,1)))
             temp_outside = float(self.get_state(self.sensor_temp_outside))
-            self.log("Temperatur aussen: {} C".format(round(temp_outside,1)))
+            #self.log("Temperatur aussen: {} C".format(round(temp_outside,1)))
         except:
             self.log("Error converting to float")
         
@@ -59,13 +59,13 @@ class humidity_manager(hass.Hass):
         DD_out = humidity_outside/100*SDD_T_out
         v_out = math.log10(DD_out/6.107)
         TD_out = (b_out*v_out)/(a_out-v_out)
-        self.log("Taupunkt aussen: {} C".format(round(TD_out,1)))
+        #self.log("Taupunkt aussen: {} C".format(round(TD_out,1)))
         SDD_TD_out = 6.1078 * math.pow(10.0, (a_out*TD_out)/(b_out+TD_out))
         SDD_T_in = 6.1078 * math.pow(10.0, (a_in*temp_inside)/(b_in+temp_inside))
         r_in = 100*SDD_TD_out/SDD_T_in
         if r_in > 100.0:
             r_in = 100.0
-        self.log("Luftfeuchtigkeit nach Lueften waere {} %".format(round(r_in,1)))
+        #self.log("Luftfeuchtigkeit nach Lueften waere {} %".format(round(r_in,1)))
         
         if humidity_inside > self.max_humidity_inside:
             if (r_in < (humidity_inside - 10)) and (r_in < self.max_humidity_inside):
