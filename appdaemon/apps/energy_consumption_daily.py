da64e14977bf435aecd01f9111d0de8b64d60c1b
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-01-05 14:05:16 +0100

    Update energy_consumption_daily.py

--- appdaemon/apps/energy_consumption_daily.py
+++ appdaemon/apps/energy_consumption_daily.py
@@ -5,17 +5,9 @@ import datetime
 #
 # Args:
 #  price_per_kWh: 0.2809
-#  db_passwd: !secret permanent_db_passwd
-#  db_field: state_float
-#  #save_measurement_name: energy_consumption_daily (if not provided, energy_consumption_test will be used)
-#  #special_date: "2020-09-30" (if provided, at app start this date will be calculated and saved to database. please remove after that one-time-calculation)
-#  db_measurement_total_kWh: sensor.stromzaehler_netzbezug (measured total consumption from power meter)
-#  db_measurements_Watt: (dict, measurement name in db : readable name for notification)
-#    sensor.el_leistung_backofen: Backofen
-#  db_measurements_kWh: (dict, measurement name in db : readable name for notification)
-#    sensor.stromzahler_kochfeld: Kochfeld
-#  permanent_consumers: (dict, device and permanent consumption in W)
-#    fritzbox: 13
+#  sensor_consumption_total: sensor.stromverbrauch_netzbezug_vortag (Power Meter)
+#  sensors_consumption_detail: (dict, sensor entity : readable name for notification)
+#    sensor.stromverbrauch_backofen_vortag: Backofen
 
 
 class energy_consumption_daily(hass.Hass):
@@ -31,20 +23,20 @@ class energy_consumption_daily(hass.Hass):
 
         # run daily
         self.run_daily(self.calculate_yesterday, daily_time)
-        self.calculate_yesterday(None) # for testing
+        self.calculate_yesterday(None) # for testing, run now
 
         
         
     def calculate_yesterday(self, kwargs):
         yesterday_str = (datetime.datetime.now() - datetime.timedelta(1)).strftime('%Y-%m-%d')
         self.log("running consumption calclulation for " + yesterday_str)
-        consumption_kWh_total = self.get_state(self.sensor_consumption_total)
+        consumption_kWh_total = float(self.get_state(self.sensor_consumption_total))
         total_consumption_cost = consumption_kWh_total * self.price_per_kWh
         
         details_dict = dict()
         known_consumption_kWh = 0.0
         for sensor in self.sensors_consumption_detail.keys():
-            consumption = self.get_state(sensor)
+            consumption = float(self.get_state(sensor))
             known_consumption_kWh = known_consumption_kWh + consumption
             if consumption > 0.01:
                 details_dict[self.sensors_consumption_detail.get(sensor)] = consumption
