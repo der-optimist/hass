2a4f2b0ebedda38ede6f7de27af53f1baf2706cc
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-03-01 14:20:37 +0100

    Update sync_target_temp_ez_wz_ku.py

--- appdaemon/apps/sync_target_temp_ez_wz_ku.py
+++ appdaemon/apps/sync_target_temp_ez_wz_ku.py
@@ -3,8 +3,9 @@ import datetime
 
 #
 # What it does:
-#   - Holds target temp. of "Wohnzimmer" and "Küche" same as "Esszimmer"
+#   - Holds target temp. of "Wohnzimmer" same as "Esszimmer"
 #     (only Esszimmer is controlled)
+#     ("Küche" removed, same KNX port as Esszimmer now)
 # What args it needs:
 #   hard coded, as only needed once
 
@@ -19,11 +20,11 @@ class sync_target_temp_ez_wz_ku(hass.Hass):
     def check_sync_state(self, kwargs):
         temp_ez = self.get_state("climate.esszimmer", attribute="temperature")
         temp_wz = self.get_state("climate.wohnzimmer", attribute="temperature")
-        temp_ku = self.get_state("climate.kuche", attribute="temperature")
+#        temp_ku = self.get_state("climate.kuche", attribute="temperature")
         
         if temp_wz != temp_ez:
             self.call_service("climate/set_temperature", entity_id = "climate.wohnzimmer", temperature = float(temp_ez))
             self.log("Target Temp. WZ differs from EZ. Set WZ to {} now".format(temp_ez))
-        if temp_ku != temp_ez:
-            self.call_service("climate/set_temperature", entity_id = "climate.kuche", temperature = float(temp_ez))
-            self.log("Target Temp. KU differs from EZ. Set KU to {} now".format(temp_ez))
+ #       if temp_ku != temp_ez:
+ #           self.call_service("climate/set_temperature", entity_id = "climate.kuche", temperature = float(temp_ez))
+ #           self.log("Target Temp. KU differs from EZ. Set KU to {} now".format(temp_ez))
