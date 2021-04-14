24456fa345d38c554c79cb5d4add8f48d833fc71
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-04-14 13:07:28 +0200

    Update test_ad.py

--- appdaemon/apps/test_ad.py
+++ appdaemon/apps/test_ad.py
@@ -3,6 +3,7 @@ import appdaemon.plugins.hass.hassapi as hass
 class test_ad(hass.Hass):
 
     def initialize(self):
+        return
         all_ha_lights = self.get_state("light")
         for light in all_ha_lights.keys():
             self.log(light)
