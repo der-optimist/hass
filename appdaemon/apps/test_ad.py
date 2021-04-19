7ad09ba8e568f272a38d2a834cd7ec9a360ede8b
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-04-19 14:59:23 +0200

    Update test_ad.py

--- appdaemon/apps/test_ad.py
+++ appdaemon/apps/test_ad.py
@@ -3,8 +3,8 @@ import appdaemon.plugins.hass.hassapi as hass
 class test_ad(hass.Hass):
 
     def initialize(self):
-#        return
-        state = self.get_state("sensor.luftfeuchtigkeit_aussen")
-        attributes = self.get_state("sensor.luftfeuchtigkeit_aussen", attribute="all")["attributes"]
-        attributes["test"] = "Test-Attribut"
-        self.set_state("sensor.luftfeuchtigkeit_aussen", state = state, attributes=attributes)
+        return
+        #state = self.get_state("sensor.luftfeuchtigkeit_aussen")
+        #attributes = self.get_state("sensor.luftfeuchtigkeit_aussen", attribute="all")["attributes"]
+        #attributes["test"] = "Test-Attribut"
+        #self.set_state("sensor.luftfeuchtigkeit_aussen", state = state, attributes=attributes)
