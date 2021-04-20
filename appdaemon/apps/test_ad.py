0014ffd9b74c7431bcca647138a2c3396066e1fa
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-04-20 09:46:25 +0200

    Update test_ad.py

--- appdaemon/apps/test_ad.py
+++ appdaemon/apps/test_ad.py
@@ -3,5 +3,5 @@ import appdaemon.plugins.hass.hassapi as hass
 class test_ad(hass.Hass):
 
     def initialize(self):
-        self.log(self.date())
+        self.log(type(self.date()))
         return
