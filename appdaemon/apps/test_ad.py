3dc9a5b2804c035ea58e8400203c06715cb4efa6
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-04-20 10:33:28 +0200

    Update test_ad.py

--- appdaemon/apps/test_ad.py
+++ appdaemon/apps/test_ad.py
@@ -3,6 +3,7 @@ import appdaemon.plugins.hass.hassapi as hass
 class test_ad(hass.Hass):
 
     def initialize(self):
-        self.log(str(self.date())[8:10])
-        self.log(str(self.date())[5:10])
+        #self.log(str(self.date())[8:10])
+        #self.log(str(self.date())[5:10])
+        self.log(self.list_namespaces())
         return
