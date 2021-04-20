16cb3227fcec604bc7f07ddd6166c23882e28276
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-04-20 10:41:41 +0200

    Update test_ad.py

--- appdaemon/apps/test_ad.py
+++ appdaemon/apps/test_ad.py
@@ -5,5 +5,6 @@ class test_ad(hass.Hass):
     def initialize(self):
         #self.log(str(self.date())[8:10])
         #self.log(str(self.date())[5:10])
-        self.set_state("sensor.test_entity", state = 0.01, attributes = {"cost_test": "1.23"}, namespace = "ad_namespace")
+        #self.set_state("sensor.test_entity", state = 0.01, attributes = {"cost_test": "1.23"}, namespace = "ad_namespace")
+        self.log(self.get_state("sensor.test_entity", namespace = "ad_namespace"))
         return
