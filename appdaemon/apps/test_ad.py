4e92560ece308e7c091b87ccd47f3dbe3341e829
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-04-23 13:49:12 +0200

    Update test_ad.py

--- appdaemon/apps/test_ad.py
+++ appdaemon/apps/test_ad.py
@@ -7,6 +7,4 @@ class test_ad(hass.Hass):
         #self.log(str(self.date())[5:10])
         #self.set_state("sensor.test_entity", state = 0.01, attributes = {"cost_test": "1.23"}, namespace = "ad_namespace")
         #self.log(self.get_state("sensor.test_entity", namespace = "ad_namespace"))
-        self.log(self.get_state("sensor.stromverbrauch_verbrauch_gesamt", attribute="all", namespace = "ad_namespace"))
-        self.log(self.get_state("sensor.stromverbrauch_waschmaschine", attribute="all", namespace = "ad_namespace"))
         return
