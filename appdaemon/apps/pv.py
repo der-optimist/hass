ba5a3cb406e0d2235e7d080456ffe57cdff1101e
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-05-19 15:07:03 +0200

    Update pv.py

--- appdaemon/apps/pv.py
+++ appdaemon/apps/pv.py
@@ -1,5 +1,6 @@
 import appdaemon.plugins.hass.hassapi as hass
 import datetime
+from influxdb import InfluxDBClient
 
 #
 # What it does:
@@ -21,30 +22,47 @@ class pv(hass.Hass):
 #        except Exception as e:
 #            self.log("Error running forecast at startup. Error was {}".format(e))
         self.run_every(self.update_forecast_regularly, "now", 15 * 60)
+        # --- initialize database stuff
+        self.host = self.app_config["global_vars"]["db_host"]
+        self.port = self.app_config["global_vars"]["db_port"]
+        self.user = self.app_config["global_vars"]["db_user"]
+        self.password = self.app_config["global_vars"]["db_passwd"]
+        self.db_name = self.app_config["global_vars"]["db_name"]
+        self.db_field_float = self.app_config["global_vars"]["db_field_float"]
+        self.client = InfluxDBClient(self.host, self.port, self.user, self.password, self.db_name)
         # --- values of today ---
-        self.ad_namespace = "ad_namespace"
-        self.sensor_name_counter_pv_produced_midnight = "sensor.counter_pv_produced_midnight"
-        self.counter_entity_pv_produced = "sensor.stromzaehler_pv_ac_gesamt"
-        self.counter_entity_pv_produced_day = "sensor.pv_erzeugung_tag"
-        self.sensor_name_counter_pv_sold_midnight = "sensor.counter_pv_sold_midnight"
-        self.counter_entity_pv_sold = "sensor.stromzaehler_netzeinspeisung"
-        self.counter_entity_pv_sold_day = "sensor.pv_netzeinspeisung_tag"
-        self.run_daily(self.save_counter_values_at_midnight, datetime.time(0, 0, 4))
+        self.ad_namespace = self.app_config["global_vars"]["ad_namespace"]
+        self.run_daily(self.save_counter_values_at_midnight, datetime.time(23, 59, 30))
         #self.save_counter_values_at_midnight(None)
         self.run_every(self.update_daily_counters, "now+10", 5 * 60)
         
     
     def save_counter_values_at_midnight(self, kwargs):
-        value_counter_pv_produced_midnight = self.get_state(self.counter_entity_pv_produced)
-        self.set_state(self.sensor_name_counter_pv_produced_midnight, state = value_counter_pv_produced_midnight, attributes = {"updated":datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')}, namespace = self.ad_namespace)
-        value_counter_pv_sold_midnight = self.get_state(self.counter_entity_pv_sold)
-        self.set_state(self.sensor_name_counter_pv_sold_midnight, state = value_counter_pv_sold_midnight, attributes = {"updated":datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')}, namespace = self.ad_namespace)
+        # calculate production of the day
+        pv_produced_day = float(self.get_state(self.args["counter_entity_pv_produced"])) - float(self.get_state(self.args["sensor_name_counter_pv_produced_midnight"], namespace = self.ad_namespace))
+        pv_sold_day = float(self.get_state(self.args["counter_entity_pv_sold"])) - float(self.get_state(self.args["sensor_name_counter_pv_sold_midnight"], namespace = self.ad_namespace))
+        # reset counters fo next day
+        value_counter_pv_produced_midnight = self.get_state(self.args["counter_entity_pv_produced"])
+        self.set_state(self.args["sensor_name_counter_pv_produced_midnight"], state = value_counter_pv_produced_midnight, attributes = {"updated":datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')}, namespace = self.ad_namespace)
+        value_counter_pv_sold_midnight = self.get_state(self.args["counter_entity_pv_sold"])
+        self.set_state(self.args["sensor_name_counter_pv_sold_midnight"], state = value_counter_pv_sold_midnight, attributes = {"updated":datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')}, namespace = self.ad_namespace)
+        # save to db
+        date_str = datetime.datetime.now().strftime('%Y-%m-%d')
+        ts_save_local_ns = datetime.datetime.strptime(date_str + 'T23:59:59.0', '%Y-%m-%dT%H:%M:%S.%f').timestamp() * 1e9
+        self.client.write_points([{"measurement":self.args["counter_entity_pv_produced_day"],"fields":{self.db_field_float:pv_produced_day},"time":int(ts_save_local_ns)}])
+        self.client.write_points([{"measurement":self.args["counter_entity_pv_sold_day"],"fields":{self.db_field_float:pv_sold_day},"time":int(ts_save_local_ns)}])
+        self.client.write_points([{"measurement":self.args["counter_entity_pv_used_day"],"fields":{self.db_field_float:(pv_produced_day - pv_sold_day)},"time":int(ts_save_local_ns)}])
+        saved_money_by_selling_day = pv_sold_day * self.get_state(self.app_config["global_vars"]["input_number_entity_compensation_per_kwh"])
+        self.client.write_points([{"measurement":self.args["entity_pv_saved_money_by_selling_day"],"fields":{self.db_field_float:saved_money_by_selling_day},"time":int(ts_save_local_ns)}])
+        saved_money_by_consuming_day = (pv_produced_day - pv_sold_day) * self.get_state(self.app_config["global_vars"]["input_number_entity_price_per_kwh"])
+        self.client.write_points([{"measurement":self.args["entity_pv_saved_money_by_consuming_day"],"fields":{self.db_field_float:saved_money_by_consuming_day},"time":int(ts_save_local_ns)}])
+        self.client.write_points([{"measurement":self.args["entity_pv_saved_money_total_day"],"fields":{self.db_field_float:(saved_money_by_selling_day + saved_money_by_consuming_day)},"time":int(ts_save_local_ns)}])
         
     def update_daily_counters(self, kwargs):
-        pv_produced_day = float(self.get_state(self.counter_entity_pv_produced)) - float(self.get_state(self.sensor_name_counter_pv_produced_midnight, namespace = self.ad_namespace))
-        pv_sold_day = float(self.get_state(self.counter_entity_pv_sold)) - float(self.get_state(self.sensor_name_counter_pv_sold_midnight, namespace = self.ad_namespace))
-        self.set_state(self.counter_entity_pv_produced_day, state = round(pv_produced_day,1), attributes = {"fiendly_name": "PV-Erzeugung heute", "icon":"mdi:counter",  "unit_of_measurement": "kWh"})
-        self.set_state(self.counter_entity_pv_sold_day, state = round(pv_sold_day,1), attributes = {"fiendly_name": "PV-Einspeisung heute", "icon":"mdi:counter",  "unit_of_measurement": "kWh"})
+        pv_produced_day = float(self.get_state(self.args["counter_entity_pv_produced"])) - float(self.get_state(self.args["sensor_name_counter_pv_produced_midnight"], namespace = self.ad_namespace))
+        pv_sold_day = float(self.get_state(self.args["counter_entity_pv_sold"])) - float(self.get_state(self.args["sensor_name_counter_pv_sold_midnight"], namespace = self.ad_namespace))
+        self.set_state(self.args["counter_entity_pv_produced_day"], state = round(pv_produced_day,1), attributes = {"fiendly_name": "PV-Erzeugung heute", "icon":"mdi:counter",  "unit_of_measurement": "kWh"})
+        self.set_state(self.args["counter_entity_pv_sold_day"], state = round(pv_sold_day,1), attributes = {"fiendly_name": "PV-Einspeisung heute", "icon":"mdi:counter",  "unit_of_measurement": "kWh"})
         
     def update_forecast_regularly(self, kwargs):
         current_forecasts = self.get_state(self.sensor_rest_forecast, attribute = "forecasts")
