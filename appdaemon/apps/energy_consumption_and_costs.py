45d9321f342009e8c44054f19e9d5131dac1c207
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-05-25 20:27:55 +0200

    Update energy_consumption_and_costs.py

--- appdaemon/apps/energy_consumption_and_costs.py
+++ appdaemon/apps/energy_consumption_and_costs.py
@@ -41,7 +41,7 @@ class energy_consumption_and_costs(hass.Hass):
         # testing
 
         # run daily
-        self.run_daily(self.generate_data_for_yesterday, time_daily_calculation)
+        #self.run_daily(self.generate_data_for_yesterday, time_daily_calculation)
         #self.run_daily(self.send_message, time_daily_message)
         #self.send_message_for_yesterday(None)
         #self.generate_data_for_yesterday(None) # Caution! Will lead to double values, if used additionally to daily calculation!
