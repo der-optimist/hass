a4f4a5ce7e4dd47a4d4c4e942e587885d5dfe877
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-06-16 10:55:28 +0200

    Update statistics_helper.py

--- appdaemon/apps/statistics_helper.py
+++ appdaemon/apps/statistics_helper.py
@@ -20,29 +20,30 @@ class statistics_helper(hass.Hass):
         self.list_of_values = []
         if self.trigger_type == "fixed_timestep":
             self.run_every(self.update_value_fixed_ts, "now+{}".format(self.startup_delay), self.time_interval_sec)
-            self.log("regularly calculation started")
+            #self.log("regularly calculation started")
         
     
     def update_value_fixed_ts(self, kwargs):
-        self.log("update triggered")
+        #self.log("update triggered")
         sensor_state = self.get_state(self.input_entity, attribute="all")
         current_value_str = sensor_state["state"]
-        if current_value_str.isnumeric():
+        try:
             current_value = float(current_value_str)
-            if len(self.list_of_values) < self.number_of_values:
-                self.list_of_values = [current_value] + self.list_of_values
-            else:
-                self.list_of_values = [current_value] + self.list_of_values[0:self.number_of_values]
-            if self.function_type == "mean":
-                output_value = sum(self.list_of_values) / len(self.list_of_values)
-            elif self.function_type == "max":
-                output_value = max(self.list_of_values)
-            elif self.function_type == "min":
-                output_value = min(self.list_of_values)
-            else:
-                self.log("unknown function type")
-            self.log("output_value: {}".format(output_value))
-            
-            self.set_state(self.output_entity, state = output_value, attributes = sensor_state["attributes"])
+        except:
+            self.log("Error converting current sensor value to float. It is: {}".format(current_value_str))
+            return
+        if len(self.list_of_values) < self.number_of_values:
+            self.list_of_values = [current_value] + self.list_of_values
         else:
-            self.log("current state of input sensor not numeric, it is: {}".format(current_value_str))
+            self.list_of_values = [current_value] + self.list_of_values[0:self.number_of_values]
+        if self.function_type == "mean":
+            output_value = sum(self.list_of_values) / len(self.list_of_values)
+        elif self.function_type == "max":
+            output_value = max(self.list_of_values)
+        elif self.function_type == "min":
+            output_value = min(self.list_of_values)
+        else:
+            self.log("unknown function type")
+        #self.log("output_value: {}".format(output_value))
+        
+        self.set_state(self.output_entity, state = output_value, attributes = sensor_state["attributes"])
