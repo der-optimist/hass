8e83ba2b12326538e25699020be89b4ea542373c
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-06-16 12:10:04 +0200

    Update pv.py

--- appdaemon/apps/pv.py
+++ appdaemon/apps/pv.py
@@ -144,10 +144,10 @@ class pv(hass.Hass):
             pv_peak = False
             self.log("Not PV peak. Forecast is {}".format(forecast_next_30_min))
         try:
-            current_pv_power = float(self.get_state(self.args["entity_pv_power"]))
+            current_pv_power_without_heating = float(self.get_state(self.args["entity_pv_power"])) + float(self.get_state("sensor.el_leistung_wp_aussenteil"))
         except:
-            current_pv_power = forecast_next_30_min
-        if pv_peak and current_pv_power >= self.args["minimum_pv_power_for_increasing_water_temp"]:
+            current_pv_power_without_heating = forecast_next_30_min
+        if pv_peak and current_pv_power_without_heating >= self.args["minimum_pv_power_for_increasing_water_temp"]:
             self.log("Will increase water target temp now")
             self.call_service("water_heater/set_operation_mode", entity_id = self.args["entity_water_heater"], operation_mode = self.args["program_hot"])
         else:
