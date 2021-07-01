b1a76c30f9bddad972d50eff64aaa3cdc2f79d5a
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-07-01 15:11:31 +0200

    Update permanent_recorder.py

--- appdaemon/apps/permanent_recorder.py
+++ appdaemon/apps/permanent_recorder.py
@@ -89,7 +89,7 @@ class permanent_recorder(hass.Hass):
         self.listen_state(self.heating_number_of_starts, "sensor.bosch_numberofstarts")
         
         # check for new entities regularly
-        #self.run_every(self.check_for_new_entities, "now+300", 5 * 60)
+        self.run_every(self.check_for_new_entities, "now+300", 5 * 60)
         
     def check_for_new_entities(self, kwargs):
         self.log("Will check for new entities")
