1382ef6ddead5ff25355c7e926be63d3c8e9674d
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-07-16 09:40:59 +0200

    Update sync_scene_and_night_entities.py

--- appdaemon/apps/sync_scene_and_night_entities.py
+++ appdaemon/apps/sync_scene_and_night_entities.py
@@ -50,13 +50,13 @@ class sync_scene_and_night_entities(hass.Hass):
         elif data["data"] == [21]:
             self.log("Lueftung hochschalten")
             # setze Laufzeit Stoßlüftung auf 30 Minuten
-            self.call_service("modbus/write_register", data = {address: 1103, unit: 1, value: 30, hub: "lueftungsanlage"})
+            self.call_service("modbus/write_register", data = {"address": 1103, "unit": 1, "value": 30, "hub": "lueftungsanlage"})
             time.sleep(1)
             # starte Stoßlüftung
-            self.call_service("modbus/write_register", data = {address: 1161, unit: 1, value: 4, hub: "lueftungsanlage"})
+            self.call_service("modbus/write_register", data = {"address": 1161, "unit": 1, "value": 4, "hub": "lueftungsanlage"})
         elif data["data"] == [22]:
             self.log("Lueftung Automatik")
-            self.call_service("modbus/write_register", data = {address: 1161, unit: 1, value: 1, hub: "lueftungsanlage"})
+            self.call_service("modbus/write_register", data = {"address": 1161, "unit": 1, "value": 1, "hub": "lueftungsanlage"})
             
     def toggle_poweroutlet_phone_ma(self,event_name,data,kwargs):
         self.log("Toggle poweroutlet phone ma")
