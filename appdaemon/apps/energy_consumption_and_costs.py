f4e7627840367a0199eab69297d406191d8d4575
der-optimist <45357614+der-optimist@users.noreply.github.com>
2022-03-23 08:29:52 +0100

    remove powercalc from AD energy calulation

--- appdaemon/apps/energy_consumption_and_costs.py
+++ appdaemon/apps/energy_consumption_and_costs.py
@@ -454,7 +454,7 @@ class energy_consumption_and_costs(hass.Hass):
         all_ha_sensors = self.get_state("sensor").keys()
         sensors_for_power_calculation = []
         for sensor_power in all_ha_sensors:
-            if sensor_power.startswith("sensor.el_leistung_") and not "scheinleistung" in sensor_power and not sensor_power.startswith("sensor.el_leistung_berechnet_licht_") and not sensor_power in self.args["db_measurements_to_skip_for_consumption_calculation"]:
+            if sensor_power.startswith("sensor.el_leistung_") and not "scheinleistung" in sensor_power and not "powercalc" in sensor_power and not sensor_power.startswith("sensor.el_leistung_berechnet_licht_") and not sensor_power in self.args["db_measurements_to_skip_for_consumption_calculation"]:
                 sensors_for_power_calculation.append(sensor_power)
         return sensors_for_power_calculation
     
